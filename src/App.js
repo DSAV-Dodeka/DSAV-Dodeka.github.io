6339a7d339cc5a556680707fafb84c78c9a1d396
Matthijs <mat.arnoldus@kpnmail.nl>
2021-02-03 23:08:44 +0100

    Basic react project

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from "./.ComCom.jpg";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Coming soon...
+        </p>
+        <a
+          className="App-link"
+          href="https://av40.nl"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          AV`40 website
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
