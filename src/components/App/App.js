240d1acfcbee730cae2fc77b6bd973a263c1d924
Matthijs <mat.arnoldus@kpnmail.nl>
2021-02-08 11:46:47 +0100

    Add simple styling

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,32 +4,29 @@ import {
     Route,
     Link
 } from "react-router-dom"
+import Navbar from "react-bootstrap/Navbar"
+import Nav from "react-bootstrap/Nav"
 import Home from '../Home/Home';
 import Nieuws from '../Nieuws/Nieuws'
 import Over from '../Over/Over'
+import './custom.scss';
 
-function NavigationBar() {
+function App() {
   return (
     <Router>
-        <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/nieuws">Nieuws</Link>
-            </li>
-            <li>
-              <Link to="/over">Over</Link>
-            </li>
-          </ul>
-        </nav>
-
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Navbar.Brand as={Link} to="/">DSAV`40</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/nieuws">Nieuws</Nav.Link>
+            <Nav.Link as={Link} to="/over">Over</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <div>
         <Switch>
-          
           <Route path="/nieuws">
             <Nieuws />
           </Route>
@@ -46,4 +43,4 @@ function NavigationBar() {
   );
 }
 
-export default NavigationBar;
+export default App;
