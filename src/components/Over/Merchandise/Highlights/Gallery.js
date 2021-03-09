0ea99a7072841a0ceda956cf04aabfa9dabc8e56
Matthijs <mat.arnoldus@kpnmail.nl>
2021-03-09 17:42:12 +0100

    Arrows instead of left/right

--- src/components/Over/Merchandise/Highlights/Gallery.js
+++ src/components/Over/Merchandise/Highlights/Gallery.js
@@ -10,12 +10,16 @@ function Gallery(props) {
 
     return(
         <div class="flex mt-8 mx-16 pt-2 pb-8 bg-white">
-            <h1 class="inline w-32 text-center self-center" onClick={() => setActiveIndex(activeIndex === 0 ? maxIndex : activeIndex - 1)}>left</h1>
+            <svg class="inline w-16 ml-16 text-center text-blauw stroke-current self-center cursor-pointer" viewBox="0 0 32 32" onClick={() => setActiveIndex(activeIndex === 0 ? maxIndex : activeIndex - 1)}>
+                <path d="M16 8 L8 16 L16 24" fill="none" stroke-width="4"/>
+            </svg>
             <div class="inline flex-grow">
                 <GalleryItem name="Windjack" price="€29,00" hidden={activeIndex === 0} image={placeholder}/>
                 <GalleryItem name="Singlet" price="€15,00" hidden={activeIndex === 1} image={droste}/>
             </div>
-            <h1 class="inline w-32 text-center self-center" onClick={() => setActiveIndex(activeIndex === maxIndex ? 0 : activeIndex + 1)}>right</h1>
+            <svg class="inline w-16 mr-16 text-center text-blauw stroke-current self-center cursor-pointer" viewBox="0 0 32 32" onClick={() => setActiveIndex(activeIndex === maxIndex ? 0 : activeIndex + 1)}>
+                <path d="M16 8 L24 16 L16 24" fill="none" stroke-width="4"/>
+            </svg>
         </div>   
     )
 }
