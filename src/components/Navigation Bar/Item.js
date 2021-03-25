b26d9e530418376d50e56c1806584a8e99aa9407
Matthijs <mat.arnoldus@kpnmail.nl>
2021-03-25 13:40:03 +0100

    Mobile navigation

--- src/components/Navigation Bar/Item.js
+++ src/components/Navigation Bar/Item.js
@@ -1,20 +1,20 @@
 import React from "react";
 import {
-    Link,
-    useLocation
+  Link,
+  useLocation
 } from "react-router-dom";
 
-const regularStyle = "absolute h-24 w-32 py-8 text-center text-white hover:bg-white hover:text-blauw text-2xl font-medium";
-const activeStyle = "absolute h-24 bg-rood text-white w-32 py-8 text-center text-2xl font-medium";
+const regularStyle = "absolute h-16 lg:h-24 w-full lg:w-32 py-4 lg:py-8 bg-blauw text-center text-white hover:bg-white hover:text-blauw text-2xl font-medium";
+const activeStyle = "absolute h-16 lg:h-24 w-full bg-rood text-white lg:w-32 py-4 lg:py-8 text-center text-2xl font-medium";
 
 function Item(props) {
-    const location = useLocation().pathname;
-    
-    return (
-      <div class="relative h-24 w-32">
+  const location = useLocation().pathname;
+
+  return (
+    <div class="relative h-16 w-full lg:h-24 lg:w-32">
       <Link to={props.path} class={location === props.path ? activeStyle : regularStyle}>{props.name}</Link>
     </div>
-    )
+  )
 }
 
 export default Item;
--- src/components/Navigation Bar/NavigationBar.js
+++ src/components/Navigation Bar/NavigationBar.js
@@ -1,20 +1,48 @@
-import React from "react";
-import Logo from "../../images/.ComCom.jpg";
+import React, { useState } from "react";
 import Item from "./Item";
 import Dropdown from "./Dropdown";
+import "./NavigationBar.css";
 
 function NavigationBar() {
+  const [active, setActive] = useState(false);
+
   return (
-    <nav class="sticky top-0 flex w-full h-24 bg-blauw mx-auto items-center z-50">
-      <h1 class="mx-16 font-bold text-white text-5xl align-text-bottom">DSAV Dodeka</h1>
-      <Item name="Home" path="/" />
-      <Item name="Nieuws" path="/nieuws" />
-      <Dropdown name="Over" path="/over" items={[{name: "Bestuur", path: "/bestuur"}, {name: "Commissies", path: "/commissies"}, {name: "Merchandise", path: "/merchandise"}, {name: "Arnold", path: "/arnold"}]}/>
-      <Item name="Trainingen" path="/trainingen" />
-      <Item name="Wedstrijden" path="/wedstrijden" />
-      <Item name="Agenda" path="/agenda" />
-      <Item name="Contact" path="/contact" />
-    </nav>
+    <div>
+      <nav class="hidden sticky top-0 lg:flex w-full h-24 bg-blauw mx-auto items-center z-50">
+        <h1 class="mx-16 font-bold text-white text-5xl align-text-bottom">DSAV Dodeka</h1>
+        <Item name="Home" path="/" />
+        <Item name="Nieuws" path="/nieuws" />
+        <Dropdown name="Over" path="/over" items={[{ name: "Bestuur", path: "/bestuur" }, { name: "Commissies", path: "/commissies" }, { name: "Merchandise", path: "/merchandise" }, { name: "Arnold", path: "/arnold" }]} />
+        <Item name="Trainingen" path="/trainingen" />
+        <Item name="Wedstrijden" path="/wedstrijden" />
+        <Item name="Agenda" path="/agenda" />
+        <Item name="Contact" path="/contact" />
+      </nav>
+      <nav class="lg:hidden sticky top-0 w-full bg-blauw z-50">
+        <div class="flex h-24 px-4 items-center">
+          <div class="inline space-y-2 cursor-pointer" onClick={() => setActive(!active)}>
+            <div class={"bg-white h-1 w-8 rounded transition duration-500" + (active ? " transform -rotate-45 translate-y-3" : "")}></div>
+            <div class={"bg-white h-1 w-8 rounded transition duration-500" + (active ? " opacity-0" : "")}></div>
+            <div class={"bg-white h-1 w-8 rounded transition duration-500" + (active ? " transform rotate-45 -translate-y-3" : "")}></div>
+          </div>
+          <div class="flex-grow"/>
+          <h1 class="mx-16 font-bold text-white text-5xl align-text-bottom">Dodeka</h1>
+          <div class="flex-grow" />
+          <div class="inline space-y-2 cursor-pointer invisible">
+            <div class={"bg-white h-1 w-8 rounded transition duration-500" + (active ? " transform -rotate-45 translate-y-3" : "")}></div>
+            <div class={"bg-white h-1 w-8 rounded transition duration-500" + (active ? " opacity-0" : "")}></div>
+            <div class={"bg-white h-1 w-8 rounded transition duration-500" + (active ? " transform rotate-45 -translate-y-3" : "")}></div>
+          </div>
+        </div>
+        <div id="mobile" onClick={() => setActive(!active)} class={"transition duration-1000 bg-blauw transform" + (active ? " h-screen" : " hidden")}>
+          <Item name="Home" path="/" />
+          <Item name="Trainingen" path="/trainingen" />
+          <Item name="Wedstrijden" path="/wedstrijden" />
+          <Item name="Agenda" path="/agenda" />
+          <Item name="Contact" path="/contact" />
+        </div>
+      </nav>
+    </div>
   )
 }
 
