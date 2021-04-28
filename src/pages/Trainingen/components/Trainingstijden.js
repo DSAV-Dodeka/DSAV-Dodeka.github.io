5703d654e522e61975f5f73bef507e75de35a209
Matthijs <mat.arnoldus@kpnmail.nl>
2021-04-28 10:53:28 +0200

    Oeps

--- src/pages/Trainingen/components/Trainingstijden.js
+++ src/pages/Trainingen/components/Trainingstijden.js
@@ -22,7 +22,7 @@ function Trainingstijden() {
             </div>
             <div class="block lg:inline bg-rood text-center text-white text-xl font-bold rounded-lg lg:rounded-none lg:rounded-t-lg py-2 w-full lg:w-40">
                 <h1>ZATERDAG</h1>
-                <h1>10:45-12:15</h1>
+                <h1>10:15-11:45</h1>
             </div>
             <div class="inline flex items-center justify-center text-center text-rood text-xl font-bold rounded-t-lg p-2 flex-grow"> 
                 <h1>ZONDAG</h1>
