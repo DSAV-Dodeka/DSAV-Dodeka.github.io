e578b71785fd8c863b3fde80bbc610e88e3ea427
Matthijs <mat.arnoldus@kpnmail.nl>
2021-04-30 20:19:56 +0200

    Paar todos weggewerkt

--- src/components/Contact Bar/ContactBar.js
+++ src/components/Contact Bar/ContactBar.js
@@ -5,7 +5,7 @@ function ContactBar() {
     return (
         <div class="w-full lg:flex bg-blauw bg-opacity-90 overflow-x-hidden">
             <div class="lg:inline w-full py-4 lg:py-8">
-                <Heading title="Contact informatie" />
+                <Heading title="Contactinformatie" />
                 <div class="pt-4 px-4 text-white lg:text-center">
                     <p>Email: studentenatletiek@av40.nl</p>
                     <p>Adres: Sportring 12, Delft</p>
@@ -14,9 +14,9 @@ function ContactBar() {
             <div class="lg:inline w-full py-4 lg:py-8">
                 <Heading title="Trainingsdagen" />
                 <div class="pt-4 px-4 text-white lg:text-center">
-                    <p>Maandag 18:00-19:30</p>
-                    <p>Woensdag 18:15-19:45</p>
-                    <p>Zaterdag 10:15-11:45</p>
+                    <p>Maandag 18:00 - 19:30</p>
+                    <p>Woensdag 18:15 - 19:45</p>
+                    <p>Zaterdag 10:15 - 11:45</p>
                 </div>
             </div>
             <div class="lg:inline w-full py-4 lg:py-8">
