c4170593e7d2e6f704aff4784fd0289d28a08352
Matthijs <mat.arnoldus@kpnmail.nl>
2021-06-05 00:40:26 +0200

    nieuws mobile

--- src/pages/Nieuws/Nieuws.js
+++ src/pages/Nieuws/Nieuws.js
@@ -9,13 +9,13 @@ function Nieuws() {
     return(
         <div class="text-center">
             <PageTitle title="Nieuws"/>
-            <div class={"space-y-24 mb-12" + (nBerichten >= NieuwsText.nieuwsberichten.length ? " mb-24" : "")}>
+            <div class={"space-y-8 lg:space-y-24 mb-4 lg:mb-12" + (nBerichten >= NieuwsText.nieuwsberichten.length ? " mb-8 lg:mb-24" : "")}>
             {NieuwsText.nieuwsberichten.slice(0, nBerichten).map((bericht, index) =>
                     <Nieuwsbericht position={index % 2 === 0 ? "left" : "right"} titel={bericht.titel} datum={bericht.datum} auteur={bericht.auteur} tekst={bericht.tekst} foto={bericht.foto}/>
                 )}
             </div>
             
-            <button onClick={() => setNBerichten(nBerichten + 3)} class={"bg-rood text-white text-xl font-bold rounded-xl p-4 mb-12" + (nBerichten >= NieuwsText.nieuwsberichten.length ? " hidden" : "")}>
+            <button onClick={() => setNBerichten(nBerichten + 3)} class={"bg-rood text-white text-xl font-bold rounded-xl py-2 px-4 lg:py-4 mb-4 lg:mb-12" + (nBerichten >= NieuwsText.nieuwsberichten.length ? " hidden" : "")}>
                 LAAD MEER
             </button>
         </div>
--- src/pages/Nieuws/components/Nieuwsbericht.js
+++ src/pages/Nieuws/components/Nieuwsbericht.js
@@ -3,15 +3,14 @@ import Title from "./Title";
 
 function Nieuwsbericht(props) {
     return (
-        <div class="relative flex">
-            {props.position === "left" ? (
-                <img class="w-1/3 object-cover" src={require(`../../../images/nieuws/${props.foto}`).default} alt=""/>) : ""}
-            <div class="w-2/3 py-8 bg-blauw bg-opacity-90">
+        <div class="relative lg:flex">
+            <img class={"w-full lg:w-1/3 object-cover" + (props.position === "right" ? " lg:hidden": "")} src={require(`../../../images/nieuws/${props.foto}`).default} alt=""/>
+            <div class="w-full lg:w-2/3 py-4 lg:py-8 bg-blauw bg-opacity-90">
                 <Title title={props.titel.toUpperCase()} position={props.position} />
-                <p class={"my-4 px-16 text-white" + (props.position === "left" ? " text-left" : " text-right")}>
+                <p class={"my-4 px-4 lg:px-16 text-white" + (props.position === "left" ? " text-left" : " text-right")}>
                     {props.datum} | {props.auteur}
                 </p>
-                <p class="my-8 px-16 text-white text-left text-base">
+                <p class="my-4 lg:my-8 px-4 lg:px-16 text-white text-left text-base">
                     {props.tekst.split('\n').map(item =>
                         <span>
                             {item}
@@ -22,7 +21,7 @@ function Nieuwsbericht(props) {
                 </p>
             </div>
             {props.position === "right" ? (
-                <img class="w-1/3 object-cover" src={require(`../../../images/nieuws/${props.foto}`).default} alt="" />) : ""}
+                <img class="w-full lg:w-1/3 object-cover hidden lg:block" src={require(`../../../images/nieuws/${props.foto}`).default} alt="" />) : ""}
         </div>
     )
 }
