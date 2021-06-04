b1c2ab5f00dc4dbaa89dabde59bf3f5c769de38d
Matthijs <mat.arnoldus@kpnmail.nl>
2021-06-04 22:28:44 +0200

    FAQ mobile

--- src/pages/Contact/components/Questions.js
+++ src/pages/Contact/components/Questions.js
@@ -3,7 +3,7 @@ import Question from "./Question";
 
 function Questions(props) {
     return (
-        <div class="grid grid-cols-2 mx-16 gap-x-8 mt-16 mb-24">
+        <div class="grid grid-cols-1 lg:grid-cols-2 mx-4 lg:mx-16 gap-x-8 mt-8 lg:mt-16 mb-8 lg:mb-24">
             {props.questions.map(vraag => 
                 <Question vraag={vraag.vraag} antwoord={vraag.antwoord}/>
             )}
