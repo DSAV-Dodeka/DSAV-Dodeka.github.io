399736ed20e171b855b6fa5b267a4717dd692ca5
Matthijs <mat.arnoldus@kpnmail.nl>
2021-06-04 19:17:47 +0200

    spacing + tekstgrootte

--- src/pages/Word lid/components/Contributie.js
+++ src/pages/Word lid/components/Contributie.js
@@ -3,16 +3,16 @@ import Header from "../../../components/Header";
 
 function Contributie() {
     return(
-        <div class="lg:flex bg-blauw bg-opacity-90 w-full mb-8 lg:mb-16">
+        <div class="lg:flex bg-blauw bg-opacity-90 w-full mb-8 lg:mb-24">
             <div class="lg:inline py-4 lg:py-0 lg:my-8 w-full lg:w-full">
                 <Header text="Contributie" position="right"/>
-                <p class="text-white text-lg mx-4 lg:mx-16 mt-4 lg:mt-8">
+                <p class="text-white text-md mx-4 lg:mx-16 mt-4 lg:mt-8">
                     Wij trainen op de atletiek baan van Atletiek Vereniging 1940. Daar zijn wij een ondervereniging van, dus daar zal dan ook de contributie naartoe gaan. <br></br><br></br>
                     Als student vallen wij onder de baanatleten, en is de contributie daarmee: 53 euro per kwartaal.
                     Om mee te mogen doen met de Nederlandse Studenten Kampioenschappen hoef je niet in het bezit te zijn van een wedstrijdlicentie,
                     maar om mee te kunnen doen aan andere wedstrijden (zoals de competitie) wel.
                     <br></br>Studentleden krijgen een korting van 10 euro per jaar op de contributie (dus 2,50 per kwartaal).
-                    Weet je het nu helemaal zeker? Je vind <a href="https://www.google.com/search?q=how+to+underline+text+in+javascript&rlz=1C1CHBD_enNL916NL916&oq=how+to+underline+text+in+javasc&aqs=chrome.0.0j69i57j0i22i30.4716j0j15&sourceid=chrome&ie=UTF-8">hier</a> het inschrijfformulier.<br></br>
+                    Weet je het nu helemaal zeker? Je vindt <a href="https://www.google.com/search?q=how+to+underline+text+in+javascript&rlz=1C1CHBD_enNL916NL916&oq=how+to+underline+text+in+javasc&aqs=chrome.0.0j69i57j0i22i30.4716j0j15&sourceid=chrome&ie=UTF-8">hier</a> het inschrijfformulier.<br></br>
                 </p>
             </div>
         </div>
--- src/pages/Word lid/components/Meetrainen.js
+++ src/pages/Word lid/components/Meetrainen.js
@@ -6,10 +6,10 @@ import ContactButtons from "../../../components/ContactButtons";
 
 function Meetrainen() {
     return (
-        <div class="lg:flex bg-blauw bg-opacity-90 w-full mb-8 lg:mb-16">
+        <div class="lg:flex bg-blauw bg-opacity-90 w-full mb-8 lg:mb-24">
             <div class="lg:inline py-4 lg:py-0 lg:my-8 w-full lg:w-1/2">
                 <Header text="Proeftrainen" position="left" />
-                <p class="text-white text-lg mx-4 lg:mx-16 mt-4 lg:mt-8">
+                <p class="text-white text-md mx-4 lg:mx-16 mt-4 lg:mt-8">
                     Bij DSAV Dodeka krijg je het hele jaar door de mogelijkheid om 3 vrijblijvend proeftrainen.
                     Zo kun je de sfeer te proeven en erachter komen of atletiek iets voor jou is.
                     Heb je na de proeftrainingen de smaak te pakken, en wil je lid bij ons worden? Klik dan <a href="https://www.google.com/search?q=how+to+underline+text+in+javascript&rlz=1C1CHBD_enNL916NL916&oq=how+to+underline+text+in+javasc&aqs=chrome.0.0j69i57j0i22i30.4716j0j15&sourceid=chrome&ie=UTF-8">hier</a> <br></br>
