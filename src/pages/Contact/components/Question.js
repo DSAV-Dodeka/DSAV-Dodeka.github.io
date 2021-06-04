3ded5f1526de512dd8f7502292afee87e46a44b0
Matthijs <mat.arnoldus@kpnmail.nl>
2021-06-04 22:36:36 +0200

    /n werkt nu wel

--- src/pages/Contact/components/Question.js
+++ src/pages/Contact/components/Question.js
@@ -4,10 +4,20 @@ function Question(props) {
     return (
         <div class=" bg-blauw bg-opacity-90 text-white rounded-xl text-lg lg:text-xl mb-8">
             <div class="w-full bg-rood rounded-t-xl py-2 px-8 font-bold">
-                {props.vraag}
+                {props.vraag.split('\n').map(item =>
+                        <span>
+                            {item}
+                            <br/>
+                        </span>
+                    )} 
             </div>
             <div class="px-8 py-2 text-base lg:text-lg">
-                {props.antwoord}
+                {props.antwoord.split('\n').map(item =>
+                        <span>
+                            {item}
+                            <br/>
+                        </span>
+                    )} 
             </div>
         </div>
     )
