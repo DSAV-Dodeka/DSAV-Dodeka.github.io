8174cf8ed5cce6d72f0e05879f08e4e8959c0956
Matthijs <mat.arnoldus@kpnmail.nl>
2021-06-04 19:37:09 +0200

    remove unused import

--- src/pages/Word lid/components/Meetrainen.js
+++ src/pages/Word lid/components/Meetrainen.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Header from "../../../components/Header";
-import image from "../../../images/placeholder.png";
 import ContactButtons from "../../../components/ContactButtons";
 
 
