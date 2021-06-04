1140dcc7ac84effcbec3ef2c005a0456a73f29c2
Matthijs <mat.arnoldus@kpnmail.nl>
2021-06-04 19:29:46 +0200

    Update structuur

--- src/pages/Word lid/WordLid.js
+++ src/pages/Word lid/WordLid.js
@@ -2,13 +2,14 @@ import React from "react";
 import PageTitle from "../../components/PageTitle";
 import Meetrainen from "./components/Meetrainen";
 import Contributie from "./components/Contributie";
+import Text from "../../content/WordLid.json";
 
 function WordLid() {
     return(
         <div>
             <PageTitle title="Word lid!"/>
-            <Meetrainen />
-            <Contributie />
+            <Meetrainen text={Text.proeftrainen.text} foto={Text.proeftrainen.foto}/>
+            <Contributie Text={Text.contributie.text} foto={Text.contributie.foto}/>
         </div>
     )
 }
--- src/pages/Word lid/components/Contributie.js
+++ src/pages/Word lid/components/Contributie.js
@@ -1,18 +1,18 @@
 import React from "react";
 import Header from "../../../components/Header";
 
-function Contributie() {
+function Contributie(props) {
     return(
         <div class="lg:flex bg-blauw bg-opacity-90 w-full mb-8 lg:mb-24">
             <div class="lg:inline py-4 lg:py-0 lg:my-8 w-full lg:w-full">
                 <Header text="Contributie" position="right"/>
                 <p class="text-white text-md mx-4 lg:mx-16 mt-4 lg:mt-8">
-                    Wij trainen op de atletiek baan van Atletiek Vereniging 1940. Daar zijn wij een ondervereniging van, dus daar zal dan ook de contributie naartoe gaan. <br></br><br></br>
-                    Als student vallen wij onder de baanatleten, en is de contributie daarmee: 53 euro per kwartaal.
-                    Om mee te mogen doen met de Nederlandse Studenten Kampioenschappen hoef je niet in het bezit te zijn van een wedstrijdlicentie,
-                    maar om mee te kunnen doen aan andere wedstrijden (zoals de competitie) wel.
-                    <br></br>Studentleden krijgen een korting van 10 euro per jaar op de contributie (dus 2,50 per kwartaal).
-                    Weet je het nu helemaal zeker? Je vindt <a href="https://www.google.com/search?q=how+to+underline+text+in+javascript&rlz=1C1CHBD_enNL916NL916&oq=how+to+underline+text+in+javasc&aqs=chrome.0.0j69i57j0i22i30.4716j0j15&sourceid=chrome&ie=UTF-8">hier</a> het inschrijfformulier.<br></br>
+                    {props.text.split('\n').map(item =>
+                        <span>
+                            {item}
+                            <br/>
+                        </span>
+                    )} 
                 </p>
             </div>
         </div>
