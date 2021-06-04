5d93547f3925ca4ea322e5ac0022a703853cfd22
Matthijs <mat.arnoldus@kpnmail.nl>
2021-06-04 18:59:54 +0200

    Trainingen structuur + tekstgroottes

--- src/pages/Trainingen/components/Icons.js
+++ src/pages/Trainingen/components/Icons.js
@@ -1,10 +1,10 @@
 import React from "react";
-import icons from "../../../images/icons.png";
+import icons from "../../../images/trainingen/icons.png";
 import "./Icons.css"
 
 function Icons() {
     return(
-        <div id="icons" class="h-20 mt-4 w-full" style={{ backgroundImage: `url(${icons})` }} />
+        <div id="icons" class="h-16 mt-8 w-full" style={{ backgroundImage: `url(${icons})` }} />
     )
 }
 export default Icons;
