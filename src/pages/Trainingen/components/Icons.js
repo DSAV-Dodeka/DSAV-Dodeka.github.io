521c6dc92e610bae26c42fc78601c0113acfb82e
Matthijs <mat.arnoldus@kpnmail.nl>
2021-06-04 21:24:02 +0200

    update mobile trainingen + word lid

--- src/pages/Trainingen/components/Icons.js
+++ src/pages/Trainingen/components/Icons.js
@@ -4,7 +4,7 @@ import "./Icons.css"
 
 function Icons() {
     return(
-        <div id="icons" class="h-20 mt-4 w-full" style={{ backgroundImage: `url(${icons})` }} />
+        <div id="icons" class="h-16 lg:h-20 lg:mt-4 w-full" style={{ backgroundImage: `url(${icons})` }} />
     )
 }
 export default Icons;
--- src/pages/Word lid/components/Contributie.js
+++ src/pages/Word lid/components/Contributie.js
@@ -3,7 +3,7 @@ import Header from "../../../components/Header";
 
 function Contributie(props) {
     return(
-        <div class="lg:flex bg-blauw bg-opacity-90 w-full mb-8 lg:mb-24">
+        <div class="lg:flex bg-blauw bg-opacity-90 w-full mb-16 lg:mb-24">
             <img src={require(`../../../images/word_lid/${props.foto}`).default} alt="" class="lg:inline w-full lg:w-1/2" />
             <div class="lg:inline py-4 lg:py-0 lg:my-8 w-full lg:w-1/2">
                 <Header text="Contributie" position="right"/>
