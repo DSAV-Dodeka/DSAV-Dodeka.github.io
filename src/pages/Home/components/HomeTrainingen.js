91ea750109744f51eb2c949b05710a9a858b3b90
1609-Jefry <61069096+1609-Jefry@users.noreply.github.com>
2021-06-05 15:58:43 +0200

    Home page content + adding fixed commissie images

--- src/pages/Home/components/HomeTrainingen.js
+++ src/pages/Home/components/HomeTrainingen.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./HomeTrainingen.css";
-import foto from "../../../images/trainingen/baanatletiek.jpg";
+import foto from "../../../images/home/trainingen.png";
 
 function HomeTrainingen() {
   return (
@@ -9,8 +9,12 @@ function HomeTrainingen() {
             <img class="w-full h-full" src={foto} alt=""/>
 
         </div>
-        <div id="traininfo" class="absolute dia w-3/4 left-0 bg-blauw bg-opacity-90 h-96 pl-40 pr-16">
-            
+        <div id="traininfo" class="absolute dia w-3/4 left-0 bg-blauw bg-opacity-90 h-96 pl-16 pr-40 pt-32 text-white">
+            De trainingen van D.S.A.V. Dodeka vinden plaats op maandag van 18:00 tot 19:30, op woensdag van 18:15 tot 19:45 en op zaterdag van 10:45 tot 12:15 op de atletiekbaan van AV`40 in Delft. Alle atletiekonderdelen, van sprint tot de marathon en van speerwerpen tot polsstokhoogspringen kunnen bij ons beoefend worden. Er trainen bij ons zowel beginnende atleten als mensen die meedoen aan NK's, dus de trainingen zijn geschikt voor elk niveau!
+            <br></br>
+            <br></br>
+            <br></br>
+            <button className="bg-rood text-white text-xl font-bold rounded-xl p-2">Lees meer!</button>
 
         </div>
         <div class="absolute left-0 top-8 w-3/4 bg-rood h-16 text-white text-right text-3xl font-bold px-16 pt-3">TRAININGEN</div>
