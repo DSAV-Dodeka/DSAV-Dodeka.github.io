b9ffefe783234f30eb69abc92b5cf4377607c159
Matthijs <mat.arnoldus@kpnmail.nl>
2021-06-05 09:16:53 +0200

    updatejes

--- src/components/Navigation Bar/Dropdown.js
+++ src/components/Navigation Bar/Dropdown.js
@@ -4,8 +4,8 @@ import {
     useLocation
 } from "react-router-dom";
 
-const regularStyle = "absolute h-16 lg:h-24 w-full lg:w-32 py-4 lg:py-8 bg-blauw text-center text-white hover:bg-white hover:text-blauw text-2xl font-medium";
-const activeStyle = "absolute h-16 lg:h-24 w-full bg-rood text-white lg:w-32 py-4 lg:py-8 text-center text-2xl font-medium";
+const regularStyle = "absolute h-16 w-full lg:w-32 py-4 bg-blauw text-center text-white hover:bg-white hover:text-blauw text-xl font-medium";
+const activeStyle = "absolute h-16 w-full bg-rood text-white lg:w-32 py-4 text-center text-xl font-medium";
 const dropdownStyle = "block h-16 py-4 w-32 text-center text-white bg-blauw text-white border-white hover:bg-white hover:text-blauw text-lg font-medium";
 
 function Dropdown(props) {
@@ -13,9 +13,9 @@ function Dropdown(props) {
     const [active, setActive] = useState(false);
 
     return (
-        <div onMouseLeave={() => setActive(false)} class="relative h-24 w-32">
+        <div onMouseLeave={() => setActive(false)} class="relative h-16 w-32">
             <Link to={props.path} onMouseEnter={() => setActive(true)} class={location.includes(props.path) ? activeStyle : regularStyle}>{props.name}</Link>
-            <div onClick={() => setActive(false)} class={active ? "absolute left-0 w-32 top-24 z-50" : "hidden"}>
+            <div onClick={() => setActive(false)} class={active ? "absolute left-0 w-32 top-16 z-50" : "hidden"}>
                 {props.items.map((item, index) => (
                     <Link to={props.path + item.path} class={index === props.items.length - 1 ? dropdownStyle + " rounded-b-md" : dropdownStyle}>{item.name}</Link>
                 ))}
