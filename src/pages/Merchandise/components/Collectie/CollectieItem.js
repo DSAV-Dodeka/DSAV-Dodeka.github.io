6cc75e85362230ce4658be57aec6b2aa5c4f9596
Matthijs <mat.arnoldus@kpnmail.nl>
2021-06-05 06:07:01 +0200

    tekst achterop

--- src/pages/Merchandise/components/Collectie/CollectieItem.js
+++ src/pages/Merchandise/components/Collectie/CollectieItem.js
@@ -10,7 +10,7 @@ function CollectieItem(props) {
                         <img class="object-fill h-64 w-64 border-4 border-blauw rounded-2xl" src={require(`../../../../images/merchandise/${props.foto}`).default} alt=""/>
                     </div>
                     <div class="back absolute bg-blauw text-white w-64 h-64 rounded-2xl text-center text-base p-4 pt-8">
-                        <h1>Dit is een geweldig shirt gemaakt van een prachtige stof. Perfect voor de trainingen en de wedstrijden.</h1>
+                        <h1>{props.text}</h1>
                     </div>
                 </div>
             </div>
