c57e44f2c993a263cea286cd81f2a478597fbec5
Matthijs <mat.arnoldus@kpnmail.nl>
2021-06-05 05:56:13 +0200

    merch structure

--- src/pages/Merchandise/components/Collectie/Collectie.js
+++ src/pages/Merchandise/components/Collectie/Collectie.js
@@ -6,9 +6,9 @@ function Collectie(props) {
     return(
         <div class="w-full my-8 font-rajdhani">
             <Title title={props.title}/>
-            <div class="grid ml-12 lg:ml-16 grid-cols-1 lg:grid-cols-3">
+            <div class="grid ml-12 lg:ml-16 grid-cols-1 lg:grid-cols-3 mb-16 lg:mb-24">
                 {props.items.map((item) => (
-                    <CollectieItem title={item.title} price={item.price} image={item.image} />
+                    <CollectieItem title={item.title} price={item.price} foto={item.image} />
                 ))}
                 
             </div>
