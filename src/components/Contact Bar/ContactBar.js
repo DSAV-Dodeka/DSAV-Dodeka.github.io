9076946afac66161038c36f114216ad9e0b343a6
Matthijs <mat.arnoldus@kpnmail.nl>
2021-06-08 16:04:58 +0200

    contact bar linkjes

--- src/components/Contact Bar/ContactBar.js
+++ src/components/Contact Bar/ContactBar.js
@@ -1,4 +1,7 @@
 import React from "react";
+import {
+    Link
+  } from "react-router-dom";
 import Heading from "./Heading";
 import {ReactComponent as Maltha} from "../../images/contact/maltha.svg";
 
@@ -6,14 +9,14 @@ function ContactBar() {
     return (
         <div class="w-full lg:flex bg-blauw bg-opacity-90 overflow-x-hidden">
             <div class="lg:inline w-full py-4 lg:py-8">
-                <Heading title="Contactinformatie" />
+                <Link to="/contact"><Heading title="Contactinformatie" /></Link>
                 <div class="pt-4 px-4 text-white lg:text-center">
-                    <p>Email: studentenatletiek@av40.nl</p>
-                    <p>Adres: Sportring 12, Delft</p>
+                    <p>Email: <a href="mailto:studentenatletiek@av40.nl" class="text-rood">studentenatletiek@av40.nl</a></p>
+                    <p>Adres: <a target="_blank" rel="noreferrer" href="https://www.google.nl/maps/place/Delftse+Atletiekvereniging+1940/@52.0294071,4.3655958,17z/data=!3m1!4b1!4m5!3m4!1s0x47c5b60b0c9dbfa9:0x9fa03ef4a72f1db8!8m2!3d52.0294038!4d4.3677845" class="text-rood">Sportring 12, Delft</a></p>
                 </div>
             </div>
             <div class="lg:inline w-full py-4 lg:py-8">
-                <Heading title="Trainingsdagen" />
+                <Link to="/trainingen"><Heading title="Trainingsdagen" /></Link>
                 <div class="pt-4 px-4 text-white lg:text-center">
                     <p>Maandag 18:00 - 19:30</p>
                     <p>Woensdag 18:15 - 19:45</p>
