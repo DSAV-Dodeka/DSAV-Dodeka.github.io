30ed607ce0f33df402342cdd95d1d0fd70e6dcb1
Matthijs <mat.arnoldus@kpnmail.nl>
2021-06-09 14:13:30 +0200

    laatste easteregg

--- src/pages/Contact/components/Questions.js
+++ src/pages/Contact/components/Questions.js
@@ -5,7 +5,7 @@ function Questions(props) {
     return (
         <div class="grid grid-cols-1 lg:grid-cols-2 mx-4 lg:mx-16 gap-x-8 mt-8 lg:mt-16 mb-8 lg:mb-24">
             {props.questions.map(vraag => 
-                <Question vraag={vraag.vraag} antwoord={vraag.antwoord}/>
+                (vraag.vraag === "Wie is de leukste mascotte?" ?  <a href="https://www.instagram.com/arnold_dodeka/?hl=en" target="_blank" rel="noreferrer"><Question vraag={vraag.vraag} antwoord={vraag.antwoord}/></a> : <Question vraag={vraag.vraag} antwoord={vraag.antwoord}/>)
             )}
         </div>
         
