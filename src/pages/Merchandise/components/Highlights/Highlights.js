d9468c65bb8337c11d93e6f42d50c46a02e1f549
1609-Jefry <61069096+1609-Jefry@users.noreply.github.com>
2021-06-15 23:08:08 +0200

    Taalfouten verbeterd + wikipedia artikel de spike toegevoegd

--- src/pages/Merchandise/components/Highlights/Highlights.js
+++ src/pages/Merchandise/components/Highlights/Highlights.js
@@ -6,7 +6,7 @@ function Highlights(props) {
     return(
         <div class="lg:flex pt-8 pb-8 mb-16 lg:mb-24 bg-blauw bg-opacity-90">
             <p class="w-full lg:w-1/2 text-white px-4 pb-4 lg:px-0 lg:pb-0 lg:mx-16">
-                Wil je als (nieuw) lid meteen de laatste D.S.A.V. Dodeka fashiontrends volgen? Bestel dan (https://forms.gle/f3RyZiA7Do4oUY696) je D.S.A.V. Dodeka merch! Op dit moment zijn de volgende kledingstukken te bestellen: T-shirt, Jack, Singlet, Hoodie, Joggingbroek. Zie hieronder de foto's voor voorbeelden.
+                Wil je als (nieuw) lid meteen de laatste D.S.A.V. Dodeka fashiontrends volgen? Bestel dan (https://forms.gle/f3RyZiA7Do4oUY696) je D.S.A.V. Dodeka merch! Op dit moment zijn de volgende kledingstukken te bestellen: T-shirt, wind-jack, singlet, hoodie en een joggingbroek. Zie hieronder de foto's voor voorbeelden.
 
                 <br></br><br></br>
                 Nu denk je vast, het lijkt wel heel erg op de oude merch, dat klopt, de nieuwe merch is in aantocht, maar de .ComCom wacht eventjes met het kiezen van de juiste foto's tot er een leuke photoshoot is gedaan met de nieuwe merch. <br></br><br></br>
