13b50ef3493030049c731a861a6f413dc2c8cc2b
Matthijs <mat.arnoldus@kpnmail.nl>
2021-07-12 14:32:21 +0200

    bug fix

--- src/pages/Word lid/components/Meetrainen.js
+++ src/pages/Word lid/components/Meetrainen.js
@@ -13,7 +13,7 @@ function Meetrainen(props) {
                 <p class="text-white text-md mx-4 lg:mx-16 mt-4 lg:mt-8">
                   Bij D.S.A.V. Dodeka krijg je het hele jaar door de mogelijkheid om drie keer gratis te komen proeftrainen. Zo kun je de sfeer te proeven en erachter komen of atletiek iets voor jou is. Heb je na de proeftrainingen de smaak te pakken en wil je lid worden? Inschrijvingen verlopen via AV`40 en hun inschrijfformulier vind je hieronder.
                 </p>
-                <a target="_blank" rel="noreferrer" href="https://www.av40.nl/index.php?page=Inschrijfformulier&sid=1" class="block relative lg:flex-1 lg:w-auto lg:w-full bg-rood py-4 text-white text-lg text-center rounded font-bold cursor-pointer mt-4 lg:mt-8 mx-4 lg:mx-16">
+                <a target="_blank" rel="noreferrer" href="https://www.av40.nl/index.php?page=Inschrijfformulier&sid=1" class="block relative lg:flex-1 lg:w-auto  bg-rood py-4 text-white text-lg text-center rounded font-bold cursor-pointer mt-4 lg:mt-8 mx-4 lg:mx-16">
                     <p>Schrijf je in!</p>
                 </a>
                 <p class="text-white text-md mx-4 lg:mx-16 mt-4 lg:mt-8">Mocht je nu nog vragen hebben, kijk dan in onze <Link smooth to="/contact#faq" class="text-rood">F.A.Q</Link>, of stuur een berichtje via de mail of Instagram!</p>
