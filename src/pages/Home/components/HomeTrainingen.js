e0fc349825576136f9ade188db12d5a9d4e8eaa3
Matthijs <mat.arnoldus@kpnmail.nl>
2021-10-07 21:11:57 +0200

    bug fix

--- src/pages/Home/components/HomeTrainingen.js
+++ src/pages/Home/components/HomeTrainingen.js
@@ -13,7 +13,7 @@ function HomeTrainingen() {
 
         </div>
         <div id="traininfo" class="lg:absolute w-full lg:w-3/4 left-0 bg-blauw bg-opacity-90 lg:h-96 p-2 lg:p-0 lg:pl-16 lg:pr-40 lg:pt-32 text-white">
-            De trainingen van D.S.A.V. Dodeka vinden plaats op maandag van 18:00 tot 19:30, op woensdag van 18:15 tot 19:45 en op zaterdag van 10:45 tot 12:15 op de atletiekbaan van AV`40 in Delft. Alle atletiekonderdelen, van sprint tot de marathon en van speerwerpen tot polsstokhoogspringen kunnen bij ons beoefend worden. Er trainen bij ons zowel beginnende atleten als mensen die meedoen aan NK's, dus de trainingen zijn geschikt voor elk niveau!
+            De trainingen van D.S.A.V. Dodeka vinden plaats op maandag van 18:00 tot 19:30, op woensdag van 18:15 tot 19:45 en op zaterdag van 10:15 tot 11:45 op de atletiekbaan van AV`40 in Delft. Alle atletiekonderdelen, van sprint tot de marathon en van speerwerpen tot polsstokhoogspringen kunnen bij ons beoefend worden. Er trainen bij ons zowel beginnende atleten als mensen die meedoen aan NK's, dus de trainingen zijn geschikt voor elk niveau!
             <br></br>
             <br></br>
             <Link to="/trainingen#"><button class="bg-rood text-white text-xl font-bold rounded-xl p-2">Lees meer!</button></Link>
