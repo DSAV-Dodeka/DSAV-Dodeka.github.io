c6e56c5740f03d91e4953bf05932308740c69043
Pien <pien.abbink@gmail.com>
2021-12-18 15:52:52 +0100

    Nieuws + Spike pagina's, LAADMEER & ruimte tussen berichten moet nog

--- nieuw/site/src/pages/Nieuws/Nieuws.js
+++ nieuw/site/src/pages/Nieuws/Nieuws.js
@@ -4,6 +4,7 @@ import Nieuwsbericht from "./components/Nieuwsbericht";
 import PageTitle from "../../components/PageTitle";
 import NieuwsText from "../../content/Nieuws.json";
 import spike from "../../images/nieuws/spike.svg";
+import "./Nieuws.scss";
 
 function Nieuws() {
     const [nBerichten, setNBerichten] = useState(3);
@@ -11,14 +12,14 @@ function Nieuws() {
     return(
         <div class="text-center">
             <PageTitle title="Nieuws"/>
-            <Link to="/nieuws/spike" class="w-4 bg-rood absolute right-1 h-0 z-30"><img src={spike} alt="" class=""/></Link>
-            <div class={"space-y-8 lg:space-y-24 mb-4 lg:mb-12" + (nBerichten >= NieuwsText.nieuwsberichten.length ? " mb-8 lg:mb-24" : "")}>
+            <Link to="/nieuws/spike" class="linktospike"><img src={spike} alt="" class=""/></Link>
+            <div class={"nieuwsberichten1" + (nBerichten >= NieuwsText.nieuwsberichten.length ? "nieuwsberichten2" : "")}>
             {NieuwsText.nieuwsberichten.slice(0, nBerichten).map((bericht, index) =>
                     <Nieuwsbericht position={index % 2 === 0 ? "left" : "right"} page="nieuws" id={bericht.id} titel={bericht.titel} datum={bericht.datum} auteur={bericht.auteur} tekst={bericht.tekst} foto={bericht.foto}/>
                 )}
             </div>
             
-            <button onClick={() => setNBerichten(nBerichten + 3)} class={"bg-rood text-white text-xl font-bold rounded-xl py-2 px-4 lg:py-4 mb-4 lg:mb-12" + (nBerichten >= NieuwsText.nieuwsberichten.length ? " hidden" : "")}>
+            <button onClick={() => setNBerichten(nBerichten + 3)} class={"nieuwsberichten3" + (nBerichten >= NieuwsText.nieuwsberichten.length ? " hidden" : "")}>
                 LAAD MEER
             </button>
         </div>
--- nieuw/site/src/pages/Nieuws/components/Nieuwsbericht.js
+++ nieuw/site/src/pages/Nieuws/components/Nieuwsbericht.js
@@ -1,16 +1,18 @@
 import React from "react";
 import Title from "./Title";
+import "./Nieuwsbericht.scss"
 
 function Nieuwsbericht(props) {
     return (
-        <div id={props.id} class="relative lg:flex">
-            <img class={"w-full lg:w-1/3 object-cover" + (props.position === "right" ? " lg:hidden": "")} src={require(`../../../images/${props.page}/${props.foto}`).default} alt=""/>
-            <div class="w-full lg:w-2/3 py-4 lg:py-8 bg-blauw bg-opacity-90">
+        <div id={props.id} class="nieuwsbericht_1">
+            {props.position === "right" ? "" : (
+                <img class="nieuwsbericht_2" src={require(`../../../images/${props.page}/${props.foto}`).default} alt="" />)}
+                <div class="nieuwsbericht_3">
                 <Title title={props.titel.toUpperCase()} position={props.position} />
-                <p class={"my-4 px-4 lg:px-16 text-white" + (props.position === "left" ? " text-left" : " text-right")}>
+                <p class={"nieuwsbericht_4" + (props.position === "left" ? " text-left" : " text-right")}>
                     {props.datum} | {props.auteur}
                 </p>
-                <p class="my-4 lg:my-8 px-4 lg:px-16 text-white text-left text-base">
+                <p class="nieuwsbericht_5">
                     {props.tekst.split('\n').map(item =>
                         <span>
                             {item}
@@ -21,7 +23,7 @@ function Nieuwsbericht(props) {
                 </p>
             </div>
             {props.position === "right" ? (
-                <img class="w-full lg:w-1/3 object-cover hidden lg:block" src={require(`../../../images/${props.page}/${props.foto}`).default} alt="" />) : ""}
+                <img class="nieuwsbericht_6" src={require(`../../../images/${props.page}/${props.foto}`).default} alt="" />) : ""}
         </div>
     )
 }
