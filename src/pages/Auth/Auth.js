6b4b135960944029cf3ae9f7760ca494debb2ea9
tiptenbrink <75669206+tiptenbrink@users.noreply.github.com>
2021-12-23 17:14:53 +0100

    Working OPAQUE including requests login/register flow

--- src/pages/Auth/Auth.js
+++ src/pages/Auth/Auth.js
@@ -74,7 +74,7 @@ async function authRequest(username, challenge) {
     return parsed.auth_id
 }
 
-async function clientRegister(password) {
+async function clientRegister(username, password) {
     const opaque = await import("@tiptenbrink/opaquewasm")
     try {
         //step 1
@@ -85,23 +85,51 @@ async function clientRegister(password) {
         console.log(message1)
         console.log(state)
 
-        // get server message from back end
+        const reqst = {
+            "username": username,
+            "client_request": message1
+        }
+        const res = await fetch(`http://localhost:4242/auth/register/start`, {
+            method: 'POST', body: JSON.stringify(reqst),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        const parsed = await res.json()
+        const server_message = parsed.server_message
+        const auth_id = parsed.auth_id
+        const register_state = state
+        console.log(auth_id)
+        console.log(server_message)
 
         // step 2
         // password 'abc'
-        const message2 = opaque.client_register_finish_wasm("HOqqdRYhiUDCYdEFt224RopWkUyh_bpAOulzXDk7CQeh0_UaT_9jsWAnAxxoxha2ZnFHQAiqLwFHKogQXblAD2FiYw==",
-            "3OoePgOwxP0k4Tqn5Cic_6EtcbKb2aAzd7EfZF0ua0j8cilJ2k3wlBxI5tG-aPV_-VNMoit3BFUK-8zO6cYpdA==")
+        //const register_state = "HOqqdRYhiUDCYdEFt224RopWkUyh_bpAOulzXDk7CQeh0_UaT_9jsWAnAxxoxha2ZnFHQAiqLwFHKogQXblAD2FiYw=="
+        //const server_message = "3OoePgOwxP0k4Tqn5Cic_6EtcbKb2aAzd7EfZF0ua0j8cilJ2k3wlBxI5tG-aPV_-VNMoit3BFUK-8zO6cYpdA=="
+        //const server_message = "Unb4-uxMjhYyrkQXpi4q0awnMMZ4k3JyTdfo9LvHfjrGVBmt60hTg5I8TzpcuB6VAZffJkgztbfI5pETN-l-WA=="
+
+        const message2 = opaque.client_register_finish_wasm(register_state, server_message)
 
         console.log(message2)
 
-        // send message to server
+        const reqst2 = {
+            "username": username,
+            "client_request": message2,
+            "auth_id": auth_id
+        }
+        const res2 = await fetch(`http://localhost:4242/auth/register/finish`, {
+            method: 'POST', body: JSON.stringify(reqst2),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
 
     } catch (e) {
         console.log(e)
     }
 }
 
-async function clientLogin(password) {
+async function clientLogin(username, password) {
     const opaque = await import("@tiptenbrink/opaquewasm")
     try {
         const arr = opaque.client_login_wasm(password)
@@ -112,14 +140,46 @@ async function clientLogin(password) {
         console.log(state)
 
         // get message to server and get message back
+        const reqst = {
+            "username": username,
+            "client_request": message1
+        }
+        const res = await fetch(`http://localhost:4242/auth/login/start`, {
+            method: 'POST', body: JSON.stringify(reqst),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        const parsed = await res.json()
+        const server_message = parsed.server_message
+        const auth_id = parsed.auth_id
+        const login_state = state
+        console.log(auth_id)
+        console.log(server_message)
 
         // pass 'abc'
-        const login_state = "Gg6GSd_2X9ccTkVZBatUyynmRM5CWBVh9j8Fsac2hQAAYoxXlNs3YTKM_4eq-Tr3hOM5TO1OZTaAgI7DYQIV4rhX-EomurCCwcw3cojfbBudPS6aF0YyxJZYbjgD8ABTigIAAMaJ77uRiMGm50uF6_VEFchFlKmwvKhhiUUsRhZhRl1fAEChX0fsJTWoEsS2bPTSt-1BKlRkL85rlA1yZkr56BWbCvhKJrqwgsHMN3KI32wbnT0umhdGMsSWWG44A_AAU4oCYWJj"
-        const server_message = "ho_5N1Kup16z2J_aoR3MxLpxrM--gE-AFLz8-bhkIh_8cilJ2k3wlBxI5tG-aPV_-VNMoit3BFUK-8zO6cYpdAETrMqI8STeP2akP4qAmQ8A5nAFshWJUpU3NfznjqXFTFPMQRJAaV9Ga-xnDUXd7KTkW18gQeoI_QWXN9xgYaFJHsYTVOYXoWKkoOwbHfurl9tNesy7DhgOnFvBH7rxH3-i3Xcl4lPuHtFFlgNCLwR4r1V0wH9tFSGC30LmXpZOBLWWZ0IXIl5BBZ5mSCJJHS9UKiYIYAHjsDjpeMQaRm_0PA70Xqrlk1dLmlhrWSoX46pZQ3Bxp2bKxF38mtr3MQcAAO3RwD2P-EutfATHdQ2W1qQZuJyOjG255FSAsbBLIOFBcpYBCNIitdoxYe7baP6gI_A9LxyK4kP0kOXg17sQ8wQ="
+        //const login_state = "Gg6GSd_2X9ccTkVZBatUyynmRM5CWBVh9j8Fsac2hQAAYoxXlNs3YTKM_4eq-Tr3hOM5TO1OZTaAgI7DYQIV4rhX-EomurCCwcw3cojfbBudPS6aF0YyxJZYbjgD8ABTigIAAMaJ77uRiMGm50uF6_VEFchFlKmwvKhhiUUsRhZhRl1fAEChX0fsJTWoEsS2bPTSt-1BKlRkL85rlA1yZkr56BWbCvhKJrqwgsHMN3KI32wbnT0umhdGMsSWWG44A_AAU4oCYWJj"
+        //const server_message = "ho_5N1Kup16z2J_aoR3MxLpxrM--gE-AFLz8-bhkIh_8cilJ2k3wlBxI5tG-aPV_-VNMoit3BFUK-8zO6cYpdAETrMqI8STeP2akP4qAmQ8A5nAFshWJUpU3NfznjqXFTFPMQRJAaV9Ga-xnDUXd7KTkW18gQeoI_QWXN9xgYaFJHsYTVOYXoWKkoOwbHfurl9tNesy7DhgOnFvBH7rxH3-i3Xcl4lPuHtFFlgNCLwR4r1V0wH9tFSGC30LmXpZOBLWWZ0IXIl5BBZ5mSCJJHS9UKiYIYAHjsDjpeMQaRm_0PA70Xqrlk1dLmlhrWSoX46pZQ3Bxp2bKxF38mtr3MQcAAO3RwD2P-EutfATHdQ2W1qQZuJyOjG255FSAsbBLIOFBcpYBCNIitdoxYe7baP6gI_A9LxyK4kP0kOXg17sQ8wQ="
+        //const server_message = "GjLrN4JEUsjQgmesadkoPWbOblKFA2B_fbgFclxoW03GVBmt60hTg5I8TzpcuB6VAZffJkgztbfI5pETN-l-WAHbuTdN1azA6NI6d-oP3TOm-_sVanwq2zE35LJAMHhXQDdLpf3YxY3OCZfMCDfjz4hC8yU9KR4kawwKnnVj8cI_DjUG2M7pFJAR5VJ1j5yYmERTn_8S_vzxm6M6y0FGARx_J8HcjATeNkdiS9DCtte-1vCZa0UnhOpOf4IEEHl3AJ71NBsDbp8kEI4GanzhH3bPCqoWukPT_MToVe1pbROJkCKaxKwBu1PuMbF4e-hw4EtQuCJmb5l6-Zm7SkowBVYAAPfgo_zRAhkBivXxX0t0H33plYrN_7yKaDZIZiCMMyiuYabsvs_op4JKgD2hV-X1PPpUdrMZ-WVrZstLRiqr2_E="
 
         const message2 = opaque.client_login_finish_wasm(login_state, server_message)
 
         console.log(message2)
+
+        const reqst2 = {
+            "username": username,
+            "client_request": message2,
+            "auth_id": auth_id
+        }
+        const res2 = await fetch(`http://localhost:4242/auth/login/finish`, {
+            method: 'POST', body: JSON.stringify(reqst2),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+
+        const session = await res2.text()
+        console.log(session)
 
     } catch (e) {
         console.log(e)
@@ -148,10 +208,10 @@ const Auth = () => {
         const code = await computeCodeVerifier()
 
         if (isRegister) {
-            await clientRegister(password)
+            await clientRegister(username, password)
         }
         else {
-            await clientLogin(password)
+            await clientLogin(username, password)
         }
 
 
