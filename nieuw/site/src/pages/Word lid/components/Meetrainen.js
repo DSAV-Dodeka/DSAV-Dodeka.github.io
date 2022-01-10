abefd8749954c09b76d3de741ed5055c1f3cd31a
Pien <pien.abbink@gmail.com>
2022-01-10 18:25:22 +0100

    Vereniging en Wordt lid: meetrainen allign schrijf in & vereniging comm& bestuur allign ook. Bij beide zijn de mail en insta knop nog te breed.

--- nieuw/site/src/pages/Word lid/components/Meetrainen.js
+++ nieuw/site/src/pages/Word lid/components/Meetrainen.js
@@ -4,22 +4,23 @@ import {
 } from "react-router-hash-link";
 import Header from "../../../components/Header";
 import ContactButtons from "../../../components/ContactButtons";
+import "./Meetrainen.scss"
 
 function Meetrainen(props) {
     return (
-        <div class="lg:flex bg-blauw bg-opacity-90 w-full mb-16 lg:mb-24">
-            <div class="lg:inline py-4 lg:py-0 lg:my-8 w-full lg:w-1/2">
+        <div class="meetrainen_1">
+            <div class="meetrainen_2">
                 <Header text="Proeftrainen" position="left" />
-                <p class="text-white text-md mx-4 lg:mx-16 mt-4 lg:mt-8">
+                <p class="meetrainen_3">
                   Bij D.S.A.V. Dodeka krijg je het hele jaar door de mogelijkheid om drie keer gratis te komen proeftrainen. Zo kun je de sfeer te proeven en erachter komen of atletiek iets voor jou is. Heb je na de proeftrainingen de smaak te pakken en wil je lid worden? Inschrijvingen verlopen via AV`40 en hun inschrijfformulier vind je hieronder.
                 </p>
-                <a target="_blank" rel="noreferrer" href="https://www.av40.nl/index.php?page=Inschrijfformulier&sid=1" class="block relative lg:flex-1 lg:w-auto bg-rood py-4 text-white text-lg text-center rounded font-bold cursor-pointer mt-4 lg:mt-8 mx-4 lg:mx-16">
+                <a target="_blank" rel="noreferrer" href="https://www.av40.nl/index.php?page=Inschrijfformulier&sid=1" class="meetrainen_4">
                     <p>Schrijf je in!</p>
                 </a>
-                <p class="text-white text-md mx-4 lg:mx-16 mt-4 lg:mt-8">Mocht je nu nog vragen hebben, kijk dan in onze <Link smooth to="/contact#faq" class="text-rood">F.A.Q.</Link>, of stuur een berichtje via de mail of Instagram!</p>
+                <p class="meetrainen_5">Mocht je nu nog vragen hebben, kijk dan in onze <Link smooth to="/contact#faq" class="meetrainen_6">F.A.Q.</Link>, of stuur een berichtje via de mail of Instagram!</p>
                 <ContactButtons />
             </div>
-            <img src={require(`../../../images/word_lid/${props.foto}`).default} alt="" class="lg:inline w-full lg:w-1/2 object-cover" />
+            <img src={require(`../../../images/word_lid/${props.foto}`).default} alt="" class="meetrainen_7" />
         </div>
     )
 }
