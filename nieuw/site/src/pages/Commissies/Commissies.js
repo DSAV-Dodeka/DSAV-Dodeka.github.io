8da0fe459c8c0a6b4c07d5d2b82316aa0ace7b5f
Matthijs <mat.arnoldus@kpnmail.nl>
2022-01-10 11:59:48 +0100

    commissies

--- nieuw/site/src/pages/Commissies/Commissies.js
+++ nieuw/site/src/pages/Commissies/Commissies.js
@@ -1,12 +1,13 @@
 import PageTitle from "../../components/PageTitle";
 import Commissie from "./components/Commissie";
 import CommissiesText from "../../content/Commissies.json";
+import "./Commissies.scss";
 
 function Commissies() {
     return (
         <div>
             <PageTitle title="Commissies" />
-            <div class="space-y-16 lg:space-y-24 overflow-x-hidden mb-16 lg:mb-24">
+            <div class="commissies overflow-x-hidden mb-16 lg:mb-24">
                 {CommissiesText.commissies.map((commissie, index) =>
                     <Commissie position={index % 2 === 0 ? "left" : "right"} name={commissie.naam} info={commissie.info} leden={commissie.leden} fotos={commissie.fotos}/>
                 )}
--- nieuw/site/src/pages/Commissies/components/Commissie.js
+++ nieuw/site/src/pages/Commissies/components/Commissie.js
@@ -1,5 +1,5 @@
 import Header from "../../../components/Header";
-import "./CommissieFlip.css";
+import "./Commissie.scss";
 
 function Commissie(props) {
     function slideIn() {
@@ -29,76 +29,76 @@ function Commissie(props) {
     }
 
     return (
-        props.position === "left" || window.innerWidth <= 768 ? (<div id={props.name} class="commissie relative lg:h-128">
-        <img class={"object-cover w-1/2 lg:w-128 lg:h-128 lg:inline-block lg:align-top lg:rounded-r-xl"} src={require(`../../../images/commissies/${props.fotos}/logo.jpg`).default} alt="" />
-        <div class="test w-1/2 relative bg-blauw bg-opacity-90 lg:inline-block h-full lg:ml-16 py-8 lg:rounded-l-xl">
-        {props.name === ".ComCom" ? <a target="_blank" rel="noreferrer" class="cursor-default" href="https://nl.wikipedia.org/wiki/Komkommer"><Header text={props.name} position="left" /></a> : <Header text={props.name} position="left" />}
-            <p class="text-lg text-white mx-4 mt-4 lg:mx-16 lg:mt-8">
+        props.position === "left" || window.innerWidth <= 1023 ? (<div id={props.name} class="commissieContainer">
+        <img class="commissieLogo roundedRight" src={require(`../../../images/commissies/${props.fotos}/logo.jpg`).default} alt="" />
+        <div class="commissieInfo roundedLeft">
+        {props.name === ".ComCom" ? <a target="_blank" rel="noreferrer" class="commissieEasteregg" href="https://nl.wikipedia.org/wiki/Komkommer"><Header text={props.name} position="left" /></a> : <Header text={props.name} position="left" />}
+            <p class="commissieStukje">
                 {props.info}
             </p>
-            <div class="hidden cursor-pointer bg-rood text-white text-xl font-bold absolute lg:flex space-x-4 right-0 top-8 p-2 h-8" onClick={() => slideIn()}>
-                <p>Bekijk de leden</p>
-                <svg id="" xmlns="http://www.w3.org/2000/svg" class=" right-4 top-0 bottom-0 my-auto w-6 h-8 fill-current" viewBox="0 0 24 24"><path d="M13.025 1l-2.847 2.828 6.176 6.176h-16.354v3.992h16.354l-6.176 6.176 2.847 2.828 10.975-11z" /></svg>
+            <div class="commissieSlider" onClick={() => slideIn()}>
+                <p class="commissieSliderMargin">Bekijk de leden</p>
+                <svg id="" xmlns="http://www.w3.org/2000/svg" class="commissieArrow" viewBox="0 0 24 24"><path d="M13.025 1l-2.847 2.828 6.176 6.176h-16.354v3.992h16.354l-6.176 6.176 2.847 2.828 10.975-11z" /></svg>
 
             </div>
         </div>
 
 
-        <div class={"align-top lg:inline-block w-full lg:w-128 lg:h-128 bg-blauw content-center lg:rounded-r-xl"}>
-            <img class="object-cover w-1/2 lg:w-128 align-center lg:rounded-r-xl" src={require(`../../../images/commissies/${props.fotos}/commissie.jpg`).default} alt="" />
+        <div class="commissieLogo roundedRight">
+            <img class="commissieFoto roundedRight" src={require(`../../../images/commissies/${props.fotos}/commissie.jpg`).default} alt="" />
         </div>
 
-        <div class="test w-1/2 relative bg-blauw bg-opacity-90 lg:inline-block h-full lg:ml-16 py-8 lg:rounded-l-xl">
+        <div class="commissieInfo roundedLeft">
             <Header text={props.name + " leden"} position="left" />
-            <div id={props.name + "scroll"} class="pt-8 pb-4 overflow-auto overflow-x-scroll whitespace-nowrap">
+            <div id={props.name + "scroll"} class="commissieLeden">
                 {props.leden.map((lid) =>
-                    <div class="h-full inline-block h-96">
-                        <img class="block h-72 mx-8" src={require(`../../../images/commissies/${props.fotos}/${lid.foto}.jpg`).default} alt="" />
-                        <p class="py-2 block text-white font-bold text-xl text-center ">{lid.naam}</p>
-                        <p class=" block text-rood font-bold text-md text-center ">{lid.functie}</p>
+                    <div class="commissieLid">
+                        <img class="commissieLidFoto" src={require(`../../../images/commissies/${props.fotos}/${lid.foto}.jpg`).default} alt="" />
+                        <p class="commissieLidNaam">{lid.naam}</p>
+                        <p class="commissieLidFunctie">{lid.functie}</p>
                     </div>
                 )}
             </div>
-            <div class="hidden cursor-pointer bg-rood text-white text-xl lg:flex space-x-4 font-bold absolute right-0 top-8 p-2 h-8 rounded-tr" onClick={() => slideOut()}>
-                <svg id="" xmlns="http://www.w3.org/2000/svg" class=" right-4 top-0 bottom-0 my-auto w-6 h-8 fill-current transform rotate-180" viewBox="0 0 24 24"><path d="M13.025 1l-2.847 2.828 6.176 6.176h-16.354v3.992h16.354l-6.176 6.176 2.847 2.828 10.975-11z" /></svg>
+            <div class="commissieSlider" onClick={() => slideOut()}>
+                <svg id="" xmlns="http://www.w3.org/2000/svg" class="commissieArrow reverseArrow commissieSliderMargin" viewBox="0 0 24 24"><path d="M13.025 1l-2.847 2.828 6.176 6.176h-16.354v3.992h16.354l-6.176 6.176 2.847 2.828 10.975-11z" /></svg>
                 <p>Bekijk de commissie</p>
             </div>
         </div>
     </div>):
     (
-            <div id={props.name} class="commissie relative lg:h-128">
-                <div class="test w-1/2 relative bg-blauw bg-opacity-90 lg:inline-block h-full mr-16 py-8 lg:rounded-r-xl">
+            <div id={props.name} class="commissieContainer">
+                <div class="commissieInfo roundedRight inverseMargin">
                     {props.name === ".ComCom" ? <a target="_blank" rel="noreferrer" class="cursor-default" href="https://nl.wikipedia.org/wiki/Komkommer"><Header text={props.name} position="left" /></a> : <Header text={props.name} position="left" />}
-                    <p class="text-lg text-white mx-4 mt-4 lg:mx-16 lg:mt-8">
+                    <p class="commissieStukje">
                         {props.info}
                     </p>
-                    <div class="cursor-pointer bg-rood text-white text-xl font-bold absolute flex space-x-4 right-0 top-8 p-2 h-8" onClick={() => slideIn()}>
-                        <p>Bekijk de leden</p>
-                        <svg id="" xmlns="http://www.w3.org/2000/svg" class="w-6 h-8 fill-current" viewBox="0 0 24 24"><path d="M13.025 1l-2.847 2.828 6.176 6.176h-16.354v3.992h16.354l-6.176 6.176 2.847 2.828 10.975-11z" /></svg>
+                    <div class="commissieSlider" onClick={() => slideIn()}>
+                        <p class="commissieSliderMargin">Bekijk de leden</p>
+                        <svg id="" xmlns="http://www.w3.org/2000/svg" class="commissieArrow" viewBox="0 0 24 24"><path d="M13.025 1l-2.847 2.828 6.176 6.176h-16.354v3.992h16.354l-6.176 6.176 2.847 2.828 10.975-11z" /></svg>
 
                     </div>
                 </div>
-                <img class={"object-cover lg:w-128 lg:h-128 lg:inline-block align-top lg:rounded-l-xl"} src={require(`../../../images/commissies/${props.fotos}/logo.jpg`).default} alt="" />
+                <img class={"commissieLogo roundedLeft"} src={require(`../../../images/commissies/${props.fotos}/logo.jpg`).default} alt="" />
 
-                <div class="test relative bg-blauw bg-opacity-90 lg:inline-block h-full mr-16 py-8 lg:rounded-r-xl">
+                <div class="commissieInfo roundedRight inverseMargin">
                     <Header text={props.name + " leden"} position="left" />
-                    <div id={props.name + "scroll"} class="pt-8 pb-4 overflow-auto overflow-x-scroll whitespace-nowrap">
+                    <div id={props.name + "scroll"} class="commissieLeden">
                         {props.leden.map((lid) =>
-                            <div class="h-full inline-block h-96">
-                                <img class="block h-72 mx-8" src={require(`../../../images/commissies/${props.fotos}/${lid.foto}.jpg`).default} alt="" />
-                                <p class="py-2 block text-white font-bold text-xl text-center ">{lid.naam}</p>
-                                <p class=" block text-rood font-bold text-md text-center ">{lid.functie}</p>
+                            <div class="commissieLid">
+                                <img class="commissieLidFoto" src={require(`../../../images/commissies/${props.fotos}/${lid.foto}.jpg`).default} alt="" />
+                                <p class="commissieLidNaam">{lid.naam}</p>
+                                <p class="commissieLidFunctie">{lid.functie}</p>
                             </div>
                         )}
                     </div>
-                    <div class="cursor-pointer bg-rood text-white text-xl flex space-x-4 font-bold absolute right-0 top-8 p-2 h-8 rounded-tr" onClick={() => slideOut()}>
-                        <svg id="" xmlns="http://www.w3.org/2000/svg" class=" right-4 top-0 bottom-0 my-auto w-6 h-8 fill-current transform rotate-180" viewBox="0 0 24 24"><path d="M13.025 1l-2.847 2.828 6.176 6.176h-16.354v3.992h16.354l-6.176 6.176 2.847 2.828 10.975-11z" /></svg>
+                    <div class="commissieSlider" onClick={() => slideOut()}>
+                        <svg id="" xmlns="http://www.w3.org/2000/svg" class="commissieArrow reverseArrow commissieSliderMargin" viewBox="0 0 24 24"><path d="M13.025 1l-2.847 2.828 6.176 6.176h-16.354v3.992h16.354l-6.176 6.176 2.847 2.828 10.975-11z" /></svg>
                         <p>Bekijk de commissie</p>
                     </div>
                 </div>
 
-                <div class={"align-top lg:inline-block w-128 h-128 bg-blauw content-center lg:rounded-l-xl"}>
-                    <img class="object-cover w-128 align-center lg:rounded-l-xl" src={require(`../../../images/commissies/${props.fotos}/commissie.jpg`).default} alt="" />
+                <div class="commissieLogo roundedLeft">
+                    <img class="commissieFoto roundedLeft" src={require(`../../../images/commissies/${props.fotos}/commissie.jpg`).default} alt="" />
                 </div>
 
 
