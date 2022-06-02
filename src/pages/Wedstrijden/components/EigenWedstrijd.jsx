5e0791087e553ac519eb8504a92ffe3b894e6980
Matthijs Arnoldus <mat.arnoldus@kpnmail.nl>
2022-06-02 13:31:18 +0200

    Oude wedstrijden grijs

--- src/pages/Wedstrijden/components/EigenWedstrijd.jsx
+++ src/pages/Wedstrijden/components/EigenWedstrijd.jsx
@@ -10,12 +10,12 @@ import getUrl from "../../../functions/links";
 
 function EigenWedstrijd(props) {
     return(
-        <div className="eigenWedstrijd">
+        <div className={"eigenWedstrijd" + (props.oud ? " eigenWedstrijdOud" : "")}>
             <p className="eigenWedstrijdNaam">{props.naam}</p>
-            <p className="eigenWedstrijdDatum">{props.datum}</p>
+            <p className={"eigenWedstrijdDatum" + (props.oud ? " eigenWedstrijdDatumOud" : "")}>{props.datum}</p>
             <img className="eigenWedstrijdFoto" src={getUrl(`wedstrijden/${props.logo}`)} alt=""/>
             <p className="eigenWedstrijdInfo">{props.info_kort}</p>
-            {props.path === "" ? "" : <Link to={"/wedstrijden" + props.path}><h1 className="eigenWedstrijdLink">Bekijk wedstrijd</h1></Link>}
+            {props.path === "" ? "" : <Link to={"/wedstrijden" + props.path}><h1 className={"eigenWedstrijdLink" + (props.oud ? " eigenWedstrijdLinkOud" : "")}>Bekijk wedstrijd</h1></Link>}
         </div>
     )
 }
