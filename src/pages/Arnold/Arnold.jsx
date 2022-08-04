24ec9c6ce80d4fbee7dc6f66e149cc8c11d349be
Matthijs Arnoldus <mat.arnoldus@kpnmail.nl>
2022-08-04 19:27:53 +0200

    Arnold info deel pc

--- src/pages/Arnold/Arnold.jsx
+++ src/pages/Arnold/Arnold.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import PageTitle from "../../components/PageTitle";
+import ArnoldInfo from "./components/ArnoldInfo";
 import "./Arnold.scss";
 
 function Arnold() {
     return(
         <div>
-            
+            <PageTitle title="Arnold"/>
+            <ArnoldInfo />
         </div>
     )
 }
---
+++ src/pages/Arnold/components/ArnoldClublied.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PageTitle from "../../components/PageTitle";
+import "./ArnoldClublied.scss";
+
+function ArnoldClublied() {
+    return(
+        <div>
+        </div>
+    )
+}
+
+export default ArnoldClublied;
