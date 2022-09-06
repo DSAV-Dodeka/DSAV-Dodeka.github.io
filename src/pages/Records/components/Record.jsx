eeae87873c5042ecc02db523780b0b2950672711
Matthijs Arnoldus <mat.arnoldus@kpnmail.nl>
2022-09-06 13:38:26 +0200

    Eerste updates mobiele versie

--- src/pages/Records/components/Record.jsx
+++ src/pages/Records/components/Record.jsx
@@ -20,7 +20,7 @@ function Record(props) {
                             </div>
                         ) : ""
                     }
-                    <svg className="arrowUp" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 16.67l2.829 2.83 9.175-9.339 9.167 9.339 2.829-2.83-11.996-12.17z"/></svg>
+                    <svg className="arrowUp recordCursor" onClick={props.onClick} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 16.67l2.829 2.83 9.175-9.339 9.167 9.339 2.829-2.83-11.996-12.17z"/></svg>
                 </div>
             </div>
             :
