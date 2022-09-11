7e0f596d3c2b0209d3706d538829d65f826951fa
Matthijs Arnoldus <mat.arnoldus@kpnmail.nl>
2022-09-11 12:57:56 +0200

    records mobile + fixes

--- src/pages/Records/components/Record.jsx
+++ src/pages/Records/components/Record.jsx
@@ -14,9 +14,9 @@ function Record(props) {
                     {
                         props.prestaties.length > 0 ? props.prestaties.map((prestatie, index) =>
                             <div className="prestatie">
-                                <p>{index + 1 + ". " + prestatie.naam}</p>
-                                <p>{prestatie.prestatie}</p>
-                                <p>{prestatie.datum + " " + prestatie.plaats}</p>
+                                <p className="prestatieNaam">{index + 1 + ". " + prestatie.naam}</p>
+                                <p className="prestatieTijd">{prestatie.prestatie}</p>
+                                <p className="pcOnly prestatieDatum">{prestatie.datum + " " + prestatie.plaats}</p>
                             </div>
                         ) : ""
                     }
@@ -25,10 +25,15 @@ function Record(props) {
             </div>
             :
             <div className={"inactiveRecord" + (props.prestaties.length > 1 ? " recordCursor": "")} onClick={props.onClick}>
-                <p>{props.onderdeel.toUpperCase()}</p>
-                <p>{props.prestaties.length > 0 ? props.prestaties[0].naam  : "Vacant"}</p>
-                <p>{props.prestaties.length > 0 ? props.prestaties[0].prestatie : ""}</p>
-                {props.prestaties.length > 1 ? <svg className="arrowDown" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 7.33l2.829-2.83 9.175 9.339 9.167-9.339 2.829 2.83-11.996 12.17z"/></svg> : ""}
+                <p className="onderdeelMobile">{props.onderdeel.toUpperCase()}</p>
+                <p className="pcOnly">{props.prestaties.length > 0 ? props.prestaties[0].naam  : "Vacant"}</p>
+                <p className="pcOnly">{props.prestaties.length > 0 ? props.prestaties[0].prestatie : ""}</p>
+                {props.prestaties.length > 1 ? <svg className="arrowDown pcOnly" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 7.33l2.829-2.83 9.175 9.339 9.167-9.339 2.829 2.83-11.996 12.17z"/></svg> : ""}
+                <div className="inactiveRecordMobile">
+                    <p className="growMobile">{props.prestaties.length > 0 ? props.prestaties[0].naam  : "Vacant"}</p>
+                    <p>{props.prestaties.length > 0 ? props.prestaties[0].prestatie : ""}</p>
+                    {props.prestaties.length > 1 ? <svg className="arrowDown" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 7.33l2.829-2.83 9.175 9.339 9.167-9.339 2.829 2.83-11.996 12.17z"/></svg> : ""}
+                </div>
             </div>
             }
         </div>
