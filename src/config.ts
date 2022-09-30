97920fa1270611b814ea543e91306ced76004989
Tip ten Brink <75669206+tiptenbrink@users.noreply.github.com>
2022-09-30 19:05:24 +0200

    Fix build mode env vars

--- src/config.ts
+++ src/config.ts
@@ -1,12 +1,11 @@
-const production = false;
 // Should be less than refresh expiry
 const max_login = 28 * 24 * 60 * 60
 
 export default
 {
-  "auth_location": import.meta.env.PAGES_AUTH_URL,
-  "client_location": import.meta.env.PAGES_CLIENT_URL,
-  "api_location": import.meta.env.PAGES_API_URL,
-  "client_id": import.meta.env.PAGES_CLIENT_ID,
+  "auth_location": import.meta.env.VITE_PAGES_AUTH_URL,
+  "client_location": import.meta.env.VITE_PAGES_CLIENT_URL,
+  "api_location": import.meta.env.VITE_PAGES_API_URL,
+  "client_id": import.meta.env.VITE_PAGES_CLIENT_ID,
   "max_login": max_login
 }
