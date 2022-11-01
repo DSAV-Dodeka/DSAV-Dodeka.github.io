09c106176075315b9cb9a8d8215336898bebb32a
Matthijs Arnoldus <mat.arnoldus@kpnmail.nl>
2022-11-01 16:49:08 +0100

    Admin alleen op pc

--- src/pages/Admin/Admin.tsx
+++ src/pages/Admin/Admin.tsx
@@ -17,23 +17,25 @@ const Admin = () => {
                 <p className="admin_status">Deze pagina is helaas niet toegankelijk als je niet ingelogd bent. Log in met een geautorizeerd account om deze pagina te kunnen bekijken.</p>
             )}
             {authState.isAuthenticated && authState.scope.includes("admin") && (
-                <div>
-                    <div className="admin_toggle">
-                        <h1 className={activeTab == "Leden" ? "admin_toggle_active" : ""} onClick={() => setActiveTab("Leden")}>Leden</h1>
-                        <h1 className={activeTab == "Aanmeldingen" ? "admin_toggle_active" : ""} onClick={() => setActiveTab("Aanmeldingen")}>Aanmeldingen</h1>
-                        <h1 className={activeTab == "Klassementen" ? "admin_toggle_active" : ""} onClick={() => setActiveTab("Klassementen")}>Klassementen</h1>
-                    </div>
-                    <div className="table_container">
-                        {
+                <>
+                    <p className="admin_status admin_mobile">Deze pagina is voorlopig alleen te gebruiken op pc.</p>
+                    <div className="admin_pc">
+                        <div className="admin_toggle">
+                            <h1 className={activeTab == "Leden" ? "admin_toggle_active" : ""} onClick={() => setActiveTab("Leden")}>Leden</h1>
+                            <h1 className={activeTab == "Aanmeldingen" ? "admin_toggle_active" : ""} onClick={() => setActiveTab("Aanmeldingen")}>Aanmeldingen</h1>
+                            <h1 className={activeTab == "Klassementen" ? "admin_toggle_active" : ""} onClick={() => setActiveTab("Klassementen")}>Klassementen</h1>
+                        </div>
+                        <div className="table_container">
                             {
-                                "Leden": <LedenInfo />,
-                                "Aanmeldingen": <ConfirmUser />,
-                                "Klassementen": <p>Coming soon</p>
-                            }[activeTab]
-                        }
+                                {
+                                    "Leden": <LedenInfo />,
+                                    "Aanmeldingen": <ConfirmUser />,
+                                    "Klassementen": <p>Coming soon</p>
+                                }[activeTab]
+                            }
+                        </div>
                     </div>
-                </div>
-                
+                </>
             )}
             {authState.isAuthenticated && !authState.scope.includes("admin") && (
                 <p className="admin_status">Deze pagina is helaas niet toegankelijk voor jouw account. Log in met een geautorizeerd account om deze pagina te kunnen bekijken.</p>
