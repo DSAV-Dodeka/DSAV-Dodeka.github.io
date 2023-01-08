92c6b0699e5476e4da0d4095c6d26b068c6fdf04
Matthijs Arnoldus <mat.arnoldus@kpnmail.nl>
2023-01-08 06:26:49 +0100

    privacy gelezen

--- src/pages/Word lid/components/SchrijfIn.tsx
+++ src/pages/Word lid/components/SchrijfIn.tsx
@@ -121,7 +121,7 @@ const SchrijfIn = () => {
                     <input type="text" name="email" placeholder="E-mail" value={state.email}
                            onChange={handleFormChange}/>
                     <div className="checkbox">
-                        <label >Ik ga akkoord met het <a href="/files/privacyverklaring_dodeka_temp.pdf" target="_blank" rel="noreferrer" className="privacy_link">privacybeleid</a></label>
+                        <label >Ik heb het <a href="/files/privacyverklaring_dodeka_temp.pdf" target="_blank" rel="noreferrer" className="privacy_link">privacybeleid</a> gelezen en ga hiermee akkoord.</label>
                         <input id="privacy" type="checkbox" name="privacy"
                                 onChange={handleCheckboxChange}/>
                     </div>
