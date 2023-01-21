44c5b7f2b752be9019504f9f105fa685e34cd20d
Tip ten Brink <75669206+tiptenbrink@users.noreply.github.com>
2023-01-21 20:01:36 +0100

    Revert "delete activiteiten"
    This reverts commit 5d4ac1b6d7f404b1ab63b38edd84bf54d3fd4480.

---
+++ src/pages/Activiteiten/components/ActiviteitAlgemeen.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import parse from "html-react-parser";
+import "../../Wedstrijden/Eigen wedstrijden/components/WedstrijdAlgemeen.scss";
+import getUrl from "../../../functions/links";
+
+
+
+type ActiviteitAlgemeen =  {
+    wedstrijd: {
+        foto: string
+        info_lang: string,
+        uitslagen?: string,
+        inschrijven?: string
+    }
+    typePath: "wedstrijden"|"vereniging/activiteiten"
+}
+function ActiviteitAlgemeen(props: ActiviteitAlgemeen) {
+    return(
+        <div className="wedstrijd_algemeen">
+            <img className="wedstrijd_algemeen_foto" alt="" src={getUrl(`${props.typePath}/${props.wedstrijd.foto}`)}/>
+            <p className="wedstrijd_algemeen_info">{parse(props.wedstrijd.info_lang)}</p>
+            {("uitslagen" in props.wedstrijd ? <a target="_blank" rel="noreferrer" href={props.wedstrijd.uitslagen} className="inschrijf_button">Uitslagen</a> : ("inschrijven" in props.wedstrijd ? <a target="_blank" rel="noreferrer" href={props.wedstrijd.inschrijven} className="inschrijf_button">Inschrijven</a> : "" ))}
+
+        </div>
+    )
+}
+
+export default ActiviteitAlgemeen;
