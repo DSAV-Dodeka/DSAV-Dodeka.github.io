300b4dc99e95f73b37dce89c337bc2ef2cab2b44
Tip ten Brink <75669206+tiptenbrink@users.noreply.github.com>
2023-04-02 18:07:06 +0200

    Update deps, test CDN for images

--- src/functions/links.ts
+++ src/functions/links.ts
@@ -1,6 +1,12 @@
 const getUrl = (loc: string) => {
-    return new URL(`../images/${loc}`, import.meta.url).href
-    //return new URL(`https://imgdodeka.b-cdn.net/${loc}`, import.meta.url).href
+    const url = new URL(`../images/${loc}`, import.meta.url)
+
+    if (import.meta.env.PROD) {
+        const path = new URL(`../images/${loc}`, import.meta.url).pathname
+        return path.replace("/assets/", "https://dodeka-1e294.kxcdn.com/")
+    } else {
+        return url.href
+    }
 }
 
 export default getUrl;
