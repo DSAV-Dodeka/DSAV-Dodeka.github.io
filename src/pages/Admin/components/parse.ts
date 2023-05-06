d091ab5fcb96a03690d5dfb4fe8e179d9df25a9e
Tip ten Brink <75669206+tiptenbrink@users.noreply.github.com>
2023-05-06 18:08:32 +0200

    Add name matcher for use in klassementen

--- src/pages/Admin/components/parse.ts
+++ src/pages/Admin/components/parse.ts
@@ -18,3 +18,69 @@ export const parseFile = <S extends ZodTypeAny>(files: FileList, rowSchema: S, r
         }
     })
 }
+
+type IdName = {
+    user_id: string,
+    name: string
+}
+
+type UserName = {
+    callname: string,
+    firstname: string,
+    lastname: string,
+    user_id: string
+}
+
+const matchNames = (users: UserName[], names: string[]) => {
+    const uniqueMatch = new Map<string, IdName>()
+    const multipleMatch = new Map<string, IdName[]>()
+    const noMatch: string[] = []
+    for (const name of names) {
+        let matched: IdName[] = []
+        let matchLevel = -1
+        for (const u of users) {
+            const firstLast = (u.firstname + " " + u.lastname)
+            const fullName = firstLast + ` (${u.callname})`
+
+            if (u.callname === name || u.firstname === name) {
+                matched.push({ user_id: u.user_id, name: fullName })
+                matchLevel = 0
+            }
+
+            if (matchLevel === 0) {
+                continue
+            }
+
+            const callLast = (u.callname + " " + u.lastname)
+
+
+            if (callLast === name || firstLast === name) {
+                matched.push({ user_id: u.user_id, name: fullName })
+                matchLevel = 1
+            }
+
+            if (matchLevel === 1) {
+                continue
+            }
+            const nameWithoutDot = name.replace('.', '')
+
+            if (callLast.includes(nameWithoutDot) || firstLast.includes(nameWithoutDot)) {
+                matched.push({ user_id: u.user_id, name: fullName })
+                matchLevel = 2
+            }
+        }
+        if (matched.length === 0) {
+            noMatch.push(name)
+        } else if (matched.length === 1) {
+            uniqueMatch.set(name, matched[0])
+        } else {
+            multipleMatch.set(name, matched)
+        }
+
+    }
+    return {
+        noMatch,
+        uniqueMatch: Object.fromEntries(uniqueMatch),
+        multipleMatch: Object.fromEntries(multipleMatch)
+    }
+}
