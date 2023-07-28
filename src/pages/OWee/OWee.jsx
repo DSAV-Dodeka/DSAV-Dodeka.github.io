c43d03e0e44c291c3bf9665d4c6c582e1481a0ca
Matthijs Arnoldus <mat.arnoldus@kpnmail.nl>
2023-07-28 15:07:04 +0200

    Add owee page + link

---
+++ src/pages/OWee/OWee.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import PageTitle from "../../components/PageTitle";
+import "./OWee.scss";
+
+function Arnold() {
+    return(
+        <div>
+            <PageTitle title="OWee"/>
+        </div>
+    )
+}
+
+export default Arnold;
