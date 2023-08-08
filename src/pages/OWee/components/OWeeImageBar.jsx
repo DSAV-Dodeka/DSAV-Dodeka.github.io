c2f9777a6b6fc82275d2af98adf35c6fc5ebf222
Matthijs Arnoldus <mat.arnoldus@kpnmail.nl>
2023-08-08 21:15:44 +0200

    Update foto's

--- src/pages/OWee/components/OWeeImageBar.jsx
+++ src/pages/OWee/components/OWeeImageBar.jsx
@@ -5,9 +5,9 @@ import getUrl from "../../../functions/links";
 function OWeeText() {
     return(
         <div className="OWeeImageBar">
-            <img className="OWeeImage" src={getUrl("owee/trainingen.jpg")}/>
+            <img className="OWeeImage" src={getUrl("owee/atletiek.jpg")}/>
             <img className="OWeeImage" src={getUrl("owee/track_festival.jpg")}/>
-            <img className="OWeeImage" src={getUrl("owee/wordLid.jpg")}/>
+            <img className="OWeeImage" src={getUrl("owee/mario_krat.JPG")}/>
             
         </div>
     )
