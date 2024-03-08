5d1ce65f5105995be6e77f47324d393b1c91efab
Matthijs Arnoldus <mat.arnoldus@kpnmail.nl>
2024-03-08 10:43:53 +0100

    Update datum herkenning in wedstrijden

--- src/pages/Wedstrijden/Wedstrijden/Wedstrijden.jsx
+++ src/pages/Wedstrijden/Wedstrijden/Wedstrijden.jsx
@@ -8,8 +8,15 @@ import TextWedstrijden from "../../../content/Wedstrijden.json";
 import records from "../../../images/wedstrijden/records.png";
 import "./Wedstrijden.scss";
 
+const dateMap = {"januari": "jan", "februari": "feb", "maart": "mar", "april": "apr", "mei": "may", "juni": "jun", "juli": "jul", "augustus": "aug", "september": "sep", "oktober": "oct", "november": "nov", "december": "dec"}
+
 export function wedstrijdGeweest(dateString) {
+    if (dateString.includes("en")) dateString = dateString.split("en")[1];
+    for (var month in dateMap) {
+        if (dateString.toLowerCase().includes(month)) dateString = dateString.replace(month, dateMap[month]);
+    }
     var date = new Date(dateString.split('/').reverse().join('-'));
+
     return date < new Date();
 }
 
