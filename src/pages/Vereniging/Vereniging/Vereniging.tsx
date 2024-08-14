8060cf8df5a8deb13ab57422e38d69df014b1c96
Tip ten Brink <75669206+tiptenbrink@users.noreply.github.com>
2024-08-14 13:56:40 +0200

    aanpassingen vereniging

--- src/pages/Vereniging/Vereniging/Vereniging.tsx
+++ src/pages/Vereniging/Vereniging/Vereniging.tsx
@@ -12,12 +12,12 @@ function Vereniging() {
         <div className="vereniging_1">
             <PageTitle title="vereniging" />
             <div className="vereniging_2">
-                <p className="vereniging_3">D.S.A.V. Dodeka is dé Delfste Studenten Atletiekgroep! Er wordt drie keer per week een training aangeboden samen met genoeg borrels, activiteiten em reizen om de leuke dodekaëders te leren kennen.<br></br><br></br>
-                    D.S.A.V. Dodeka is aangesloten bij de Nederlandse Studenten Atletiek Federatie ZeuS.
+                <p className="vereniging_3">Dodeka is dé Delfste Studenten Atletiekgroep! Er wordt drie keer per week een training aangeboden samen met genoeg borrels, activiteiten em reizen om de leuke dodekaëders te leren kennen.<br></br><br></br>
+                    Dodeka is aangesloten bij de Nederlandse Studenten Atletiek Federatie ZeuS.
 
                     De atleten van Dodeka zijn niet bang voor wat competitie en gaan vaak in groepjes naar allerlei verschillende wedstrijden toe. De grootste opkomst is te vinden bij alle Nederlands Studenten Kampioenschappen (NSK) waar wij altijd aan meedoen.<br></br><br></br>
 
-                    Wij zijn een jonge vereniging met zo'n 160 leden. Dat ledenaantal heeft ons niet tegengehouden met het opzetten van de velen commissies. Er zijn genoeg commissies waar jij een bijdrage kan leveren, en van kan leren. Er is bijvoorbeeld een commissie voor activiteiten, maar ook voor de website waar je nu op kijkt, ook zijn er commissies voor de borrels of voor de nieuwsbrief en nog meer!<br></br><br></br>
+                    Wij zijn een jonge groep met zo'n 160 leden. Dat ledenaantal heeft ons niet tegengehouden met het opzetten van de velen commissies. Er zijn genoeg commissies waar jij een bijdrage kan leveren, en van kan leren. Er is bijvoorbeeld een commissie voor activiteiten, maar ook voor de website waar je nu op kijkt, ook zijn er commissies voor de borrels of voor de nieuwsbrief en nog meer!<br></br><br></br>
 
                    <b>Geschiedenis</b> <br></br>
 
