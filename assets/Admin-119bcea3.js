e8541952da30d8b2db9508b028f6b2c2b2cbb800
JespervdM <150844865+JespervdM@users.noreply.github.com>
2024-12-02 20:57:37 +0000

    Update site to output generated at e91c7b7

--- assets/Admin-119bcea3.js
+++ assets/Admin-119bcea3.js
@@ -1,4 +1,4 @@
-import{r as E,j as s,h as _t,i as At,l as Et,R as Mt,z as Ge}from"./vendor-44375bc4.js";import{A as Fe,b as Se,c as Ie,P as It}from"./index-3a0ca3d6.js";import{c as Pt,q as je,d as Dt,e as Vt,f as Lt,g as Ot,h as zt}from"./queries-55eaff50.js";import{R as xe}from"./Rollen-83d067d1.js";import{E as Ht,I as ke}from"./date-028b19b5.js";/**
+import{r as E,j as s,h as _t,i as At,l as Et,R as Mt,z as Ge}from"./vendor-44375bc4.js";import{A as Fe,b as Se,c as Ie,P as It}from"./index-cc772a09.js";import{c as Pt,q as je,d as Dt,e as Vt,f as Lt,g as Ot,h as zt}from"./queries-8777387b.js";import{R as xe}from"./Rollen-83d067d1.js";import{E as Ht,I as ke}from"./date-028b19b5.js";/**
  * table-core
  *
  * Copyright (c) TanStack
