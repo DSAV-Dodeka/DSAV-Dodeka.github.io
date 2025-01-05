a77f190380294bbc23a89a270be4557bfe1c547a
Matthijs Arnoldus <mat.arnoldus@kpnmail.nl>
2025-01-05 15:31:43 +0100

    Revert "22-8"
    This reverts commit 07994d024de0e8fc38641beba37bf6039f0f0062.

--- src/components/Contact Bar/ContactBar.jsx
+++ src/components/Contact Bar/ContactBar.jsx
@@ -16,7 +16,7 @@ function ContactBar() {
                     <p>Email: <a href="mailto:bestuur@dsavdodeka.nl" className="bar_link">bestuur@dsavdodeka.nl</a></p>
                     <p>Adres: <a target="_blank" rel="noreferrer" href="https://www.google.nl/maps/place/Delftse+Atletiekvereniging+1940/@52.0294071,4.3655958,17z/data=!3m1!4b1!4m5!3m4!1s0x47c5b60b0c9dbfa9:0x9fa03ef4a72f1db8!8m2!3d52.0294038!4d4.3677845" className="bar_link">Sportring 12, Delft</a></p>
                     <Link to="/contact" className="bar_link">F.A.Q.</Link><br/>
-                    <a href="/files/privacyverklaring_dodeka_jan23.pdf" target="_blank" rel="noreferrer" className="privacy_link">Privacyverklaring Dodeka</a>
+                    <a href="/files/privacyverklaring_dodeka_jan23.pdf" target="_blank" rel="noreferrer" className="privacy_link">Privacyverklaring D.S.A.V. Dodeka</a>
                 </div>
             </div>
             <div className="bar_container">
