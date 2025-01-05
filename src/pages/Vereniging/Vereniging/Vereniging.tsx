b76821adc2e66d7d29f7f680c1f1e777b35d4ff7
Matthijs Arnoldus <mat.arnoldus@kpnmail.nl>
2025-01-05 15:29:13 +0100

    Revert "aanpassingen vereniging"
    This reverts commit 8060cf8df5a8deb13ab57422e38d69df014b1c96.

--- src/pages/Vereniging/Vereniging/Vereniging.tsx
+++ src/pages/Vereniging/Vereniging/Vereniging.tsx
@@ -12,12 +12,12 @@ function Vereniging() {
         <div className="vereniging_1">
             <PageTitle title="wie zijn wij" />
             <div className="vereniging_2">
-                <p className="vereniging_3">D.S.A.V. Dodeka is dé Delfste Studenten AtletiekVereniging! Er wordt drie keer per week een training aangeboden samen met genoeg borrels, activiteiten em reizen om de leuke dodekaëders te leren kennen.<br></br><br></br>
+                <p className="vereniging_3">D.S.A.V. Dodeka is dé Delfste Studenten Atletiekgroep! Er wordt drie keer per week een training aangeboden samen met genoeg borrels, activiteiten em reizen om de leuke dodekaëders te leren kennen.<br></br><br></br>
                     D.S.A.V. Dodeka is aangesloten bij de Nederlandse Studenten Atletiek Federatie ZeuS.
 
                     De atleten van Dodeka zijn niet bang voor wat competitie en gaan vaak in groepjes naar allerlei verschillende wedstrijden toe. De grootste opkomst is te vinden bij alle Nederlands Studenten Kampioenschappen (NSK) waar wij altijd aan meedoen.<br></br><br></br>
 
-                    Wij zijn een jonge groep met zo'n 160 leden. Dat ledenaantal heeft ons niet tegengehouden met het opzetten van de velen commissies. Er zijn genoeg commissies waar jij een bijdrage kan leveren, en van kan leren. Er is bijvoorbeeld een commissie voor activiteiten, maar ook voor de website waar je nu op kijkt, ook zijn er commissies voor de borrels of voor de nieuwsbrief en nog meer!<br></br><br></br>
+                    Wij zijn een jonge vereniging met zo'n 160 leden. Dat ledenaantal heeft ons niet tegengehouden met het opzetten van de velen commissies. Er zijn genoeg commissies waar jij een bijdrage kan leveren, en van kan leren. Er is bijvoorbeeld een commissie voor activiteiten, maar ook voor de website waar je nu op kijkt, ook zijn er commissies voor de borrels of voor de nieuwsbrief en nog meer!<br></br><br></br>
 
                    <b>Geschiedenis</b> <br></br>
                     Dodeka is op 25 februari 2019 begonnen onder de naam DSAV'40 als een commissie bij AV'40. In 2021 is de naam gewijzigd naar Dodeka.</p>
