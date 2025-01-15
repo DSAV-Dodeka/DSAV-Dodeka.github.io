834a5a311157dd2fd5898b19ced9fe46f2c4af70
tiptenbrink <75669206+tiptenbrink@users.noreply.github.com>
2025-01-15 18:06:54 +0000

    Update site to output generated at fd5e730

--- assets/Admin-Bc14N9_g.js
+++ assets/Admin-Bc14N9_g.js
@@ -1,4 +1,4 @@
-import{r as I,j as o,g as Ct,h as $t,R as It,z as Ge}from"./vendor-CK6RSgI1.js";import{A as ye,b as Se,c as De,P as Pt}from"./index-DUu6Zk8c.js";import{c as Dt,q as ke,d as Vt,e as At,f as Lt,g as Ot,h as zt}from"./queries-h_bYPmUc.js";import{R as we}from"./Rollen-CKONlSLe.js";import{E as Nt,I as xe}from"./date-C0_92tuL.js";/**
+import{r as I,j as o,g as Ct,h as $t,R as It,z as Ge}from"./vendor-CK6RSgI1.js";import{A as ye,b as Se,c as De,P as Pt}from"./index-EtRTrefm.js";import{c as Dt,q as ke,d as Vt,e as At,f as Lt,g as Ot,h as zt}from"./queries-Cu2MxR5Y.js";import{R as we}from"./Rollen-CKONlSLe.js";import{E as Nt,I as xe}from"./date-C0_92tuL.js";/**
    * table-core
    *
    * Copyright (c) TanStack
