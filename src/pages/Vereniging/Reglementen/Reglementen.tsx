d1978e2e436852ba675a86c46552eac29e7116c1
Matthijs Arnoldus <mat.arnoldus@kpnmail.nl>
2025-10-11 16:28:04 +0200

    Merge fix

--- src/pages/Vereniging/Reglementen/Reglementen.tsx
+++ src/pages/Vereniging/Reglementen/Reglementen.tsx
@@ -1,28 +1,10 @@
 // import React, {useContext, useEffect, useState} from "react";
 import "./Reglementen.scss";
-import soon from "$images/leden/soon.jpg";
+import PageTitle from "../../../components/PageTitle";
 
 
 function Reglementen() {
         return(
-<<<<<<< HEAD
-            <div className="reglementen-container">
-                <div className="leden_link">
-                        {/* <img src={soon} className="leden_link_image" alt=""/> */}
-                        <a href="/files/DSAV_Dodeka_Statuten.pdf" target="_blank" rel="noreferrer" className="leden_link_header">Statuten</a>
-                </div>
-                <div className="leden_link">
-                        {/* <img src={soon} className="leden_link_image" alt=""/> */}
-                        <a href="/files/Huishoudelijk_Reglement.pdf" target="_blank" rel="noreferrer" className="leden_link_header">Huishoudelijk Reglement</a>
-                </div>
-                <div className="leden_link">
-                        {/* <img src={soon} className="leden_link_image" alt=""/> */}
-                        <a href="/files/Trainers_gedragscode.pdf" target="_blank" rel="noreferrer" className="leden_link_header">Trainers gedragscode</a>
-                </div>
-                <div className="leden_link">
-                        {/* <img src={soon} className="leden_link_image" alt=""/> */}
-                        <a href="/files/Bestuur_gedragscode.pdf" target="_blank" rel="noreferrer" className="leden_link_header">Bestuur gedragscode</a>
-=======
             <div>
                 <PageTitle title="Reglementen" />
                 <div className="reglementen-container">
@@ -40,7 +22,6 @@ function Reglementen() {
                             <a href="/files/Bestuur_gedragscode.pdf" target="_blank" rel="noreferrer" className="leden_link_header">Bestuur gedragscode</a>
                     </div>
                   </div>
->>>>>>> reglementen_update
                 </div>
             </div>
         )
