35c67b4e2f0a7328b13a57925919c08447b4238f
Matthijs Arnoldus <mat.arnoldus@kpnmail.nl>
2025-10-11 16:20:02 +0200

    Update reglementenpagina

--- src/pages/Vereniging/Reglementen/Reglementen.tsx
+++ src/pages/Vereniging/Reglementen/Reglementen.tsx
@@ -7,22 +7,23 @@ import {getNestedImagesUrl} from "../../../functions/links";
 
 function Reglementen() {
         return(
-            <div className="reglementen-container">
-                <div className="leden_link">
-                        {/* <img src={getNestedImagesUrl("leden/soon.jpg")} className="leden_link_image" alt=""/> */}
-                        <a href="/files/DSAV_Dodeka_Statuten.pdf" target="_blank" rel="noreferrer" className="leden_link_header">Statuten</a>
-                </div>
-                <div className="leden_link">
-                        {/* <img src={getNestedImagesUrl("leden/soon.jpg")} className="leden_link_image" alt=""/> */}
-                        <a href="/files/Huishoudelijk_Reglement.pdf" target="_blank" rel="noreferrer" className="leden_link_header">Huishoudelijk Reglement</a>
-                </div>
-                <div className="leden_link">
-                        {/* <img src={getNestedImagesUrl("leden/soon.jpg")} className="leden_link_image" alt=""/> */}
-                        <a href="/files/Trainers_gedragscode.pdf" target="_blank" rel="noreferrer" className="leden_link_header">Trainers gedragscode</a>
-                </div>
-                <div className="leden_link">
-                        {/* <img src={getNestedImagesUrl("leden/soon.jpg")} className="leden_link_image" alt=""/> */}
-                        <a href="/files/Bestuur_gedragscode.pdf" target="_blank" rel="noreferrer" className="leden_link_header">Bestuur gedragscode</a>
+            <div>
+                <PageTitle title="Reglementen" />
+                <div className="reglementen-container">
+                  <div className="reglementen-grid">
+                    <div className="leden_link">
+                            <a href="/files/DSAV_Dodeka_Statuten.pdf" target="_blank" rel="noreferrer" className="leden_link_header">Statuten</a>
+                    </div>
+                    <div className="leden_link">
+                            <a href="/files/Huishoudelijk_Reglement.pdf" target="_blank" rel="noreferrer" className="leden_link_header">Huishoudelijk Reglement</a>
+                    </div>
+                    <div className="leden_link">
+                            <a href="/files/Trainers_gedragscode.pdf" target="_blank" rel="noreferrer" className="leden_link_header">Trainers gedragscode</a>
+                    </div>
+                    <div className="leden_link">
+                            <a href="/files/Bestuur_gedragscode.pdf" target="_blank" rel="noreferrer" className="leden_link_header">Bestuur gedragscode</a>
+                    </div>
+                  </div>
                 </div>
             </div>
         )
