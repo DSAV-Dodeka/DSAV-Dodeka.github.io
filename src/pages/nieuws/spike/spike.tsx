97c03256662e31575236ec6be29cf043acb2ebed
Mei Chang van der Werff <m.c.l.werff@student.tudelft.nl>
2026-02-27 12:13:54 +0100

    stage de rest

--- src/pages/nieuws/spike/spike.tsx
+++ src/pages/nieuws/spike/spike.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Nieuwsbericht from "../nieuws/components/Nieuwsbericht";
 import SpikeText from "../../../content/Spike.json";
 import "./Spike.scss";
-import logo from "$images/spike/logo.png";
+import logo from "$images/spike/logo.webp";
 
 function Spike() {
   const [nBerichten, setNBerichten] = useState(3);
