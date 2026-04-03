8eb6f7288d6b4b7c786ffcdd8832781d24cb20e4
Anne-Wil van Werkhoven <annewilvanwerkhkoven@gmail.com>
2026-04-04 00:14:54 +0200

    trainingen knop aangepast

--- src/pages/home/components/HomeTrainingen.tsx
+++ src/pages/home/components/HomeTrainingen.tsx
@@ -15,7 +15,7 @@ function HomeTrainingen() {
             De trainingen van Dodeka vinden plaats op maandag van 18:15 tot 19:45, op woensdag van 18:15 tot 19:45 en op zaterdag van 10:15 tot 11:45 op de atletiekbaan in Delft. Alle atletiekonderdelen, van sprint tot de marathon en van speerwerpen tot polsstokhoogspringen kunnen bij ons beoefend worden. Er trainen bij ons zowel beginnende atleten als mensen die meedoen aan NK's, dus de trainingen zijn geschikt voor elk niveau!
             <br></br>
             <br></br>
-            <Link to="/trainingen#"><button id="home_trainingen_button">Lees meer!</button></Link>
+            <Link to="/trainingen#"><button id="home_trainingen_button">Lees meer over de trainingen!</button></Link>
 
         </div>
         <div id="home_trainingen_title">TRAININGEN</div>
