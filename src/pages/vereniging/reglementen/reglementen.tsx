2a416416b05155a96dccfe62bbbf5a2aaf89a1f7
XylanderParqui <xylanderparqui@gmail.com>
2026-04-04 03:09:34 +0200

    Reglementen pagina aangepast

--- src/pages/vereniging/reglementen/reglementen.tsx
+++ src/pages/vereniging/reglementen/reglementen.tsx
@@ -1,33 +1,104 @@
 // import React, {useContext, useEffect, useState} from "react";
 import "./reglementen.scss";
+import plaatje from "../../../images/vereniging/reglementen/lijnen3.png"
 import PageTitle from "../../../components/PageTitle";
 
 
 function Reglementen() {
-        return(
-            <div>
-                <PageTitle title="Reglementen" />
-                <div className="reglementen-container">
-                  <div className="reglementen-grid">
-                    <div className="leden_link">
-                            <a href="/files/DSAV_Dodeka_Statuten.pdf" target="_blank" rel="noreferrer" className="leden_link_header">Statuten</a>
+    return (
+        <div>
+            <PageTitle title="Reglementen" />
+            <div className="reglementen-container">
+                {/* Achtergrond SVG */}
+                <img className="aha" src={plaatje} alt="achtergrond" />
+
+                <div className="reglementen-grid">
+                    <div className="leden_link_wrapper">
+                        <div className="leden_link_shadow"></div>
+                        <div className="leden_link">
+                            <a
+                                href="/files/DSAV_Dodeka_Statuten.pdf"
+                                target="_blank"
+                                rel="noreferrer"
+                                className="leden_link_header"
+                            >
+                                Statuten
+                            </a>
+                        </div>
                     </div>
-                    <div className="leden_link">
-                            <a href="/files/DSAV_Dodeka_Huishoudelijk_Reglement.pdf" target="_blank" rel="noreferrer" className="leden_link_header">Huishoudelijk Reglement</a>
+
+                    <div className="leden_link_wrapper">
+                        <div className="leden_link_shadow"></div>
+                        <div className="leden_link">
+                            <a
+                                href="/files/DSAV_Dodeka_Huishoudelijk_Reglement.pdf"
+                                target="_blank"
+                                rel="noreferrer"
+                                className="leden_link_header"
+                            >
+                                Huishoudelijk Reglement
+                            </a>
+                        </div>
                     </div>
-                    <div className="leden_link">
-                            <a href="/files/Trainers_gedragscode.pdf" target="_blank" rel="noreferrer" className="leden_link_header">Trainers gedragscode</a>
+
+                    <div className="leden_link_wrapper">
+                        <div className="leden_link_shadow"></div>
+                        <div className="leden_link">
+                            <a
+                                href="/files/Trainers_gedragscode.pdf"
+                                target="_blank"
+                                rel="noreferrer"
+                                className="leden_link_header"
+                            >
+                                Trainers gedragscode
+                            </a>
+                        </div>
                     </div>
-                    <div className="leden_link">
-                            <a href="/files/Bestuur_gedragscode.pdf" target="_blank" rel="noreferrer" className="leden_link_header">Bestuur gedragscode</a>
+
+                    <div className="leden_link_wrapper">
+                        <div className="leden_link_shadow"></div>
+                        <div className="leden_link">
+                            <a
+                                href="/files/Bestuur_gedragscode.pdf"
+                                target="_blank"
+                                rel="noreferrer"
+                                className="leden_link_header"
+                            >
+                                Bestuur gedragscode
+                            </a>
+                        </div>
                     </div>
-                    <div className="leden_link">
-                            <a href="/files/DSAV_Dodeka_Gedragscode.pdf" target="_blank" rel="noreferrer" className="leden_link_header">Leden gedragscode</a>
+
+                    <div className="leden_link_wrapper">
+                        <div className="leden_link_shadow"></div>
+                        <div className="leden_link">
+                            <a
+                                href="/files/DSAV_Dodeka_Gedragscode.pdf"
+                                target="_blank"
+                                rel="noreferrer"
+                                className="leden_link_header"
+                            >
+                                Leden gedragscode
+                            </a>
+                        </div>
                     </div>
-                  </div>
+                    <div className="leden_link_wrapper">
+                        <div className="leden_link_shadow"></div>
+                        <div className="leden_link">
+                            <a
+                                href="/files/privacyverklaring_dodeka_okt25.pdf"
+                                target="_blank"
+                                rel="noreferrer"
+                                className="leden_link_header"
+                            >
+                                Privacy verklaring
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </div>
-        )
+        </div>
+    );
 }
 
 export default Reglementen;
